500ba001b7453d4a2852ddcd974e4a38d583c7f0
Michael Bulinski <michael.bulinski@tu-dortmund.de>
2014-04-11 15:21:47 +0200

    testing stuff and more

--- src/test/java/fact/io/zfits/ZFitsFileTest.java
+++ src/test/java/fact/io/zfits/ZFitsFileTest.java
@@ -1,6 +1,6 @@
 package fact.io.zfits;
 
-import static org.junit.Assert.*;
+//import static org.junit.Assert.*;
 
 import java.io.IOException;
 
@@ -12,7 +12,7 @@ import stream.util.parser.ParseException;
 public class ZFitsFileTest {
 	@Test
 	public void headerTest() throws IOException, ParseException, FitsFileException {
-		String filename = ZFitsFileTest.class.getResource("/zfits_test.fz").getFile();
-		ZFitsFile file = new ZFitsFile(filename);
+		//String filename = ZFitsFileTest.class.getResource("/zfits_test.fz").getFile();
+		new ZFitsFile("/home/tarrox/physik/fact/data/20130331_013.fits.fz");
 	}
 }
