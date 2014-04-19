5be05e83b5039aa7a8c280996b2d0c170bb59ee2
Maximilian Noethe <maximilian.noethe@udo.edu>
2014-04-19 15:40:43 +0200

    Fixed a forgotten sqrt

--- src/main/java/fact/features/MyonHoughTransform.java
+++ src/main/java/fact/features/MyonHoughTransform.java
@@ -2,9 +2,14 @@ package fact.features;
 
 import stream.Data;
 import stream.Processor;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
+
+import fact.Constants;
+import fact.image.Pixel;
+import fact.image.overlays.PixelSet;
 
 
 
@@ -13,15 +18,18 @@ public class MyonHoughTransform implements Processor {
 	private String peaknessKey;
 	private String distanceKey;
 	private String ringKey;
-	//private String photonChargeKey;
+	private String bestCircleKey;
+	private String photonChargeKey;
+	private String showRingKey;
+
 	
 	private double min_radius = 80; //minimal radius in mm
-	private double max_radius = 120; //maximal radius in mm
+	private double max_radius = 140; //maximal radius in mm
 	private double min_x = -300; //minimal center X in mm
 	private double max_x = 300; //maximal center X in mm
 	private double min_y = -300; //minimal center y in mm
 	private double max_y = 300; //maximal center y in mm
-	private int res_r = 10;
+	private int res_r = 18;
 	private int res_x = 60;
 	private int res_y = 60;
 	
@@ -34,14 +42,8 @@ public class MyonHoughTransform implements Processor {
 		
 				
 		int[] ring = (int[]) input.get(ringKey);
-		
-		if (ring.length<20){
-			log.info(String.valueOf(ring.length));
-			input.put(distanceKey, "None");
-			input.put(peaknessKey, "None");
-			return input;
-		}
-		
+		double[] photonCharge = (double[]) input.get(photonChargeKey);
+				
 		//double[] charge = (double[]) input.get(photonchargeKey);
 		
 		double[] xPositions = new double[ring.length];
@@ -87,14 +89,14 @@ public class MyonHoughTransform implements Processor {
 		int[] second_highest_pos = {0, 0, 0};
 		int[] third_highest_pos = {0, 0, 0};
 		
-		
+				
 		for (int r = 0; r < circle_radius.length; r++){
 			for (int x = 0; x < circle_x.length; x++){
 				for (int y = 0; y < circle_y.length; y++){
 					for(int pix = 0; pix < ring.length; pix++){
-						double distance = Math.pow((xPositions[pix] - circle_x[x]), 2.0) + Math.pow((yPositions[pix] - circle_y[y]), 2.0);
+						double distance = Math.sqrt(Math.pow((xPositions[pix] - circle_x[x]), 2.0) + Math.pow((yPositions[pix] - circle_y[y]), 2.0));
 						if(Math.abs(distance - circle_radius[r]) < 1.1 * fact.Constants.PIXEL_SIZE ){
-							HoughMatrix[r][x][y] += 1;
+							HoughMatrix[r][x][y] += photonCharge[ring[pix]];
 						}
 						stats.addValue(HoughMatrix[r][x][y]);
 						if (HoughMatrix[r][x][y] !=0){
@@ -112,11 +114,16 @@ public class MyonHoughTransform implements Processor {
 						highest_pos[1] = x;
 						highest_pos[2] = y;
 					}
+					
 				}
 			}
 		}
 		
-		//log.info("HoughTransform durchgefügt");
+		
+		
+		
+		
+        //log.info("HoughTransform durchgefügt");
 		
 		
 
@@ -124,16 +131,16 @@ public class MyonHoughTransform implements Processor {
 		
 		
 		double radius_1 = circle_radius[highest_pos[0]];
-		double center_y_1 = circle_x[highest_pos[1]];
-		double center_x_1 = circle_y[highest_pos[2]];
-		
+		double center_x_1 = circle_x[highest_pos[1]];
+		double center_y_1 = circle_y[highest_pos[2]];
+				
 		double radius_2 = circle_radius[second_highest_pos[0]];
-		double center_y_2 = circle_x[second_highest_pos[1]];
-		double center_x_2 = circle_y[second_highest_pos[2]];
+		double center_x_2 = circle_x[second_highest_pos[1]];
+		double center_y_2 = circle_y[second_highest_pos[2]];
 		
 		double radius_3 = circle_radius[third_highest_pos[0]];
-		double center_y_3 = circle_x[third_highest_pos[1]];
-		double center_x_3 = circle_y[third_highest_pos[2]];
+		double center_x_3 = circle_x[third_highest_pos[1]];
+		double center_y_3 = circle_y[third_highest_pos[2]];
 		
 		double ParamDistance1 = Math.sqrt(Math.pow(radius_1 - radius_2, 2) + Math.pow(center_x_1 - center_x_2, 2) + Math.pow(center_y_1 - center_y_2 , 2));
 		double ParamDistance2 = Math.sqrt(Math.pow(radius_1 - radius_3, 2) + Math.pow(center_x_1 - center_x_3, 2) + Math.pow(center_y_1 - center_y_3 , 2));
@@ -147,8 +154,28 @@ public class MyonHoughTransform implements Processor {
 		
 		peakness = HoughMaximum/(HoughSum/NoneZeroElems);
 		
+		
+		
+		//if (showRingKey=="true"){
+//			double PixelPosX;
+//			double PixelPosY;
+//			double distance;
+//			PixelSet bestCirclePixelSet = new PixelSet();
+//			
+//			for (int pix=0; pix<Constants.NUMBEROFPIXEL; pix++){
+//				PixelPosX = fact.viewer.ui.DefaultPixelMapping.getPosXinMM(pix);
+//				PixelPosY = fact.viewer.ui.DefaultPixelMapping.getPosYinMM(pix);
+//				distance = Math.sqrt(Math.pow((PixelPosX - center_x_1), 2.0) + Math.pow((PixelPosY - center_y_1), 2.0));
+//				if(Math.abs(distance - radius_1) < 1.1 * fact.Constants.PIXEL_SIZE ){
+//					bestCirclePixelSet.add(new Pixel(pix));
+//				}
+//			}
+//			input.put(bestCircleKey, bestCirclePixelSet);
+		//}
+		
 		input.put(peaknessKey, peakness);
 		input.put(distanceKey, ParamDistanceSum);
+		
 		
 		return input;
 	
@@ -167,16 +194,28 @@ public class MyonHoughTransform implements Processor {
 	public void setPeaknessKey(String peaknessKey) {
 		this.peaknessKey = peaknessKey;
 	}
+	public String getBestCircleKey() {
+		return bestCircleKey;
+	}
+	public void setBestCircleKey(String bestCircleKey) {
+		this.bestCircleKey = bestCircleKey;
+	}
 	public String getRingKey() {
 		return ringKey;
 	}
 	public void setRingKey(String ringKey) {
 		this.ringKey = ringKey;
 	}
-//	public String getPhotonChargeKey() {
-//		return photonChargeKey;
-//	}
-//	public void setPhotonChargeKey(String photonChargeKey) {
-//		this.photonChargeKey = photonChargeKey;
-//	}
+	public String getShowRingKey() {
+		return showRingKey;
+	}
+	public void setShowRing(String showRingKey) {
+		this.showRingKey = showRingKey;
+	}
+	public String getPhotonChargeKey() {
+		return photonChargeKey;
+	}
+	public void setPhotonChargeKey(String photonChargeKey) {
+		this.photonChargeKey = photonChargeKey;
+	}
 }
