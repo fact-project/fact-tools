4a09511d307e83a47d24a57a18875347ef4ffd1d
Michael Bulinski <michael.bulinski@tu-dortmund.de>
2014-04-28 12:15:33 +0200

    removed test

--- src/test/java/fact/io/zfits/ZFitsFileTest.java
+++ src/test/java/fact/io/zfits/ZFitsFileTest.java
@@ -4,12 +4,9 @@ package fact.io.zfits;
 
 import java.io.IOException;
 
-import org.junit.Test;
-
 import stream.util.parser.ParseException;
 
 public class ZFitsFileTest {
-	@Test
 	public void headerTest() throws IOException, ParseException {
 		//String filename = ZFitsFileTest.class.getResource("/zfits_test.fz").getFile();
 		new ZFitsFile("/home/tarrox/physik/fact/data/20130331_013.fits.fz");
