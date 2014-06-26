99da9449712dce12179bb37f0957de7a1b1a8cac
Fabian Temme <fabian.temme@tu-dortmund.de>
2014-06-26 15:59:33 +0200

    Added a EventUtilsTest

---
+++ src/test/java/fact/utils/EventUtilsTest.java
@@ -0,0 +1,32 @@
+package fact.utils;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import fact.EventUtils;
+
+public class EventUtilsTest {
+
+	@Test
+	public void testPointToShowerCoord(){
+		double cogx = 5.0;
+		double cogy = 7.0;
+		double x = 15.0;
+		double y = 7 + Math.tan(30.0/180.0*Math.PI)*10.0;
+		double delta = -45.0/180.0*Math.PI;
+		double[] pointRot = {0,0};
+		pointRot[0] = Math.cos(-15.0/180.0*Math.PI)*10.0/Math.cos(30.0/180.0*Math.PI);
+		pointRot[1] = Math.sin(-15.0/180.0*Math.PI)*10.0/Math.cos(30.0/180.0*Math.PI);
+		
+		double[] result = EventUtils.rotatePointInShowerSystem(x, y, cogx, cogy, delta);
+		
+		double diff = Math.abs( Math.sqrt( Math.pow(pointRot[0]-result[0],2) + Math.pow(pointRot[1]-result[1],2) ) );
+		
+		assertTrue("Rotation of point into shower coordinates didn't worked.\nCalculated result: " +
+				 result[0] + "," + result[1] + "\n correct Result: " +
+				 pointRot[0] + "," + pointRot[1],diff < 1E-3 );
+		
+	}
+
+}
