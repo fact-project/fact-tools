def927e41f500bb200603a20958e5bf2dbd375f9
Fabian Temme <fabian.temme@tu-dortmund.de>
2014-06-26 15:59:21 +0200

    Added a first version of a probability cleaning and a time gradient extraction, not yet finished

---
+++ src/main/java/fact/cleaning/ProbabilityClean.java
@@ -0,0 +1,118 @@
+package fact.cleaning;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import fact.Constants;
+import fact.EventUtils;
+import fact.viewer.ui.DefaultPixelMapping;
+import stream.Data;
+import stream.Processor;
+
+public class ProbabilityClean implements Processor {
+
+	private String photonChargeKey = null;
+	
+	private String outputKey = null;
+	
+	private String deltaKey = null;
+	private String cogxKey = null;
+	private String cogyKey = null;
+	
+	private double probabilityThreshold;
+	
+	private double[] photoncharge = null;
+	private double delta;
+	private double cogx;
+	private double cogy;
+	
+	@Override
+	public Data process(Data input) {
+		EventUtils.mapContainsKeys(getClass(), input, photonChargeKey, deltaKey);
+		
+		photoncharge = (double[]) input.get(photonChargeKey);
+		
+		delta = (Double) input.get(deltaKey);
+		
+		cogx = (Double) input.get(cogxKey);
+		cogy = (Double) input.get(cogyKey);
+		
+		List<Integer> showerlist = new ArrayList<Integer>();
+		
+		for (int px = 0 ; px < Constants.NUMBEROFPIXEL ; px++)
+		{
+			double xpos = DefaultPixelMapping.getPosXinMM(px);
+			double ypos = DefaultPixelMapping.getPosYinMM(px);
+			double weight = photoncharge[px] / CalculateDistance(px,xpos,ypos);
+			
+			if (weight > probabilityThreshold)
+			{
+				showerlist.add(px);
+			}
+			
+		}
+		if (showerlist.size() > 0)
+		{
+			Integer[] showerArray = new Integer[showerlist.size()];
+			showerlist.toArray(showerArray);
+			
+			input.put(outputKey, showerArray);
+		}
+		
+		return input;
+	}
+
+	private double CalculateDistance(int px, double xpos, double ypos) {
+		
+		return 1;
+	}
+
+	public String getPhotonChargeKey() {
+		return photonChargeKey;
+	}
+
+	public void setPhotonChargeKey(String photonChargeKey) {
+		this.photonChargeKey = photonChargeKey;
+	}
+
+	public String getOutputKey() {
+		return outputKey;
+	}
+
+	public void setOutputKey(String outputKey) {
+		this.outputKey = outputKey;
+	}
+
+	public String getDeltaKey() {
+		return deltaKey;
+	}
+
+	public void setDeltaKey(String deltaKey) {
+		this.deltaKey = deltaKey;
+	}
+
+	public String getCogxKey() {
+		return cogxKey;
+	}
+
+	public void setCogxKey(String cogxKey) {
+		this.cogxKey = cogxKey;
+	}
+
+	public String getCogyKey() {
+		return cogyKey;
+	}
+
+	public void setCogyKey(String cogyKey) {
+		this.cogyKey = cogyKey;
+	}
+
+	public double getProbabilityThreshold() {
+		return probabilityThreshold;
+	}
+
+	public void setProbabilityThreshold(double probabilityThreshold) {
+		this.probabilityThreshold = probabilityThreshold;
+	}
+
+}
---
+++ src/main/java/fact/features/TimeGradientExtraction.java
@@ -0,0 +1,173 @@
+package fact.features;
+
+import java.net.URL;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import fact.Constants;
+import fact.EventUtils;
+import fact.viewer.ui.DefaultPixelMapping;
+import stream.Data;
+import stream.Processor;
+import stream.annotations.Parameter;
+import stream.io.CsvStream;
+import stream.io.SourceURL;
+
+public class TimeGradientExtraction implements Processor {
+	static Logger log = LoggerFactory.getLogger(TimeGradientExtraction.class);
+
+	private String deltaKey = null;
+	private String cogxKey = null;
+	private String cogyKey = null;
+	
+	private String dataKey = null;
+	
+	private String timeGradientKey = null;
+	
+	private String outputKey = null;
+	
+	private int searchWindowSize;
+	
+	@Parameter(required = true, description = "The url to the inputfiles for the gain calibration constants",defaultValue="file:src/main/resources/defaultIntegralGains.csv")
+    private URL url = null;
+	
+	Data integralGainData = null;
+    private double[] integralGains = new double[Constants.NUMBEROFPIXEL];
+	
+	private int integralSize = 30;
+	
+	private double[] result = null;
+	
+	private double delta;
+	private double cogx;
+	private double cogy;
+	private double timeGradient;
+	private int roi = 0;
+	
+	private double[] data = null;
+	
+	
+	@Override
+	public Data process(Data input) {
+		
+		EventUtils.mapContainsKeys(getClass(), input, deltaKey,cogxKey,cogyKey,dataKey);
+		
+		data = (double[]) input.get(dataKey);
+		
+		delta = (Double) input.get(deltaKey);
+		
+		cogx = (Double) input.get(cogxKey);
+		cogy = (Double) input.get(cogyKey);
+		
+		timeGradient = (Double) input.get(timeGradientKey);
+		
+		roi = (Integer) input.get("NROI");
+		
+		result = new double[Constants.NUMBEROFPIXEL];
+		
+		for (int px = 0 ; px < Constants.NUMBEROFPIXEL ; px++)
+		{
+			double posx = DefaultPixelMapping.getPosXinMM(px);
+			double posy = DefaultPixelMapping.getPosYinMM(px);
+			int predictedTime = CalculatePredictedTime(px,posx,posy);
+			int leftBorder = predictedTime - searchWindowSize / 2;
+			int rightBorder = predictedTime + searchWindowSize / 2;
+			if (searchWindowSize%2 == 1)
+			{
+				rightBorder += 1;
+			}
+			int maxPos = CalculateMaxPosition(px,leftBorder,rightBorder);
+			result[px] = CalculateIntegral(px,maxPos,leftBorder) / integralGains[px];
+		}
+		
+		
+		input.put(outputKey, result);
+		
+		return input;
+	}
+
+
+	private double CalculateIntegral(int px, int maxPos, int leftBorder) {
+		int leftIntegralBorder = maxPos;
+		
+		for (; leftIntegralBorder > leftBorder ; leftIntegralBorder--)
+		{
+			int pos = px * roi + leftIntegralBorder;
+			if (data[pos] < data[maxPos] / 2.0)
+			{
+				break;
+			}
+		}
+		
+		double integral = 0;
+		
+		for (int sl = leftIntegralBorder ; sl < leftIntegralBorder + integralSize ; sl++)
+		{
+			int pos = px*roi + sl;
+			integral += data[pos];
+		}
+		
+		return integral;
+	}
+
+
+	private int CalculateMaxPosition(int px, int leftBorder, int rightBorder) {
+		int maxPos = 0;
+		double tempMax = Double.MIN_VALUE;
+		
+		for (int sl = leftBorder ; sl < rightBorder ; sl++)
+		{
+			int pos = px * roi + sl;
+			if (data[pos] > tempMax)
+			{
+				maxPos = sl;
+				tempMax = data[pos];
+			}
+		}
+		
+		
+		return maxPos;
+	}
+
+
+	private int CalculatePredictedTime(int px, double posx, double posy) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	public void setUrl(URL url) {
+		try {
+			loadIntegralGainFile(new SourceURL(url));
+		} catch (Exception e) {
+			throw new RuntimeException(e.getMessage());
+		}
+		this.url = url;
+	}
+
+
+	public URL getUrl() {
+		return url;
+	}
+
+
+	private void loadIntegralGainFile(SourceURL inputUrl) {
+		try {
+			CsvStream stream = new CsvStream(inputUrl, " ");
+			stream.setHeader(false);
+			stream.init();
+			integralGainData = stream.readNext();
+			
+			for (int i = 0 ; i < Constants.NUMBEROFPIXEL ; i++){
+				String key = "column:" + (i);
+				this.integralGains[i] = (Double) integralGainData.get(key);
+			}
+			
+		} catch (Exception e) {
+			log.error("Failed to load integral Gain data: {}", e.getMessage());
+			e.printStackTrace();
+		}
+		
+	}
+
+}
