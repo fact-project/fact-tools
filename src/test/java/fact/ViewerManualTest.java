a88a5ebab42e55a8c12449c6c276c520aca6f89a
Kai Brügge <kai.bruegge@edu.udo>
2014-06-30 17:00:55 +0200

    updated viewertest to new cleaning interface

--- src/test/java/fact/ViewerManualTest.java
+++ src/test/java/fact/ViewerManualTest.java
@@ -20,7 +20,8 @@ public class ViewerManualTest {
             URL url = ViewerManualTest.class.getResource("/viewertest.xml");
             URL drsUrl =  ViewerManualTest.class.getResource("/testDrsFile.drs.fits.gz");
             URL dataUrl =  ViewerManualTest.class.getResource("/testDataFile.fits.gz");
-            String[] args = {url.toString(), "-Dinput="+dataUrl.toString(), "-DdrsInput="+drsUrl.toString()};
+            URL driveUrl =  ViewerManualTest.class.getResource("/testDriveFile.fits");
+            String[] args = {url.toString(), "-Dinput="+dataUrl.toString(), "-DdrsInput="+drsUrl.toString(), "-DdriveFile="+driveUrl.toString()};
             stream.run.main(args);
         } catch (Exception e) {
             System.out.println(e.getMessage());
