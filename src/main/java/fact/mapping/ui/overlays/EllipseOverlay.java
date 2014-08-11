2b8d5a525d877054f6bb33ef0051f7363df1b52f
Kai Brügge <kai.bruegge@edu.udo>
2014-08-11 16:27:26 +0200

    Ellipse now painting correctly

--- src/main/java/fact/mapping/ui/overlays/EllipseOverlay.java
+++ src/main/java/fact/mapping/ui/overlays/EllipseOverlay.java
@@ -1,6 +1,5 @@
 package fact.mapping.ui.overlays;
 
-import fact.mapping.FactPixelMapping;
 import fact.mapping.ui.components.cameradisplay.FactHexMapDisplay;
 
 import java.awt.*;
@@ -9,36 +8,24 @@ import java.awt.geom.Ellipse2D;
 import java.io.Serializable;
 
 /**
+ * Draws an ellipse on the cameraview. The cameraview is rotated by 90 degrees. So X values correspond
+ * to the horizontal axis in the GUI
  * Created by bruegge on 7/31/14.
  */
 public class EllipseOverlay implements CameraMapOverlay, Serializable {
 
+    private final double cogY;
+    private final double cogX;
+    private final double ellipse_height;
+    private final double ellipse_width;
     private Color fillColor = Color.GRAY;
-    private Color strokeColor = Color.red;
     private double angle = 0;
-    private double ellipseCenterX = 0; //in screen pixels
-    private double ellipseCenterY = 0; //in screen pixels
-    private Shape el;
-    FactPixelMapping mappping = FactPixelMapping.getInstance();
 
-    public EllipseOverlay(double cogX, double cogY, double width, double length, double angle){
-        //el = new Ellipse2D.Double(cogX - width*0.5, cogY - length*0.5, width, length);
-
-        double scalingx = 1.21;
-        double scalingy = 1.27;
-
-        double offsetx = cogX*scalingx;
-        double offsety = -cogY*scalingy;//(millimeter)
-        double w = 3*width;
-        double h = 3*length;
-
-
-        this.ellipseCenterX = offsetx;
-        this.ellipseCenterY = offsety;
-
-        System.out.println("prinitng ellipse at pixels: " +ellipseCenterX + ", " + ellipseCenterY);
-
-        el = new Ellipse2D.Double((0-w*0.5) + offsetx, (0-h*0.5) + offsety, w, h);
+    public EllipseOverlay(double cogX, double cogY, double width, double height, double angle){
+        this.cogX = cogX;
+        this.cogY = cogY;
+        this.ellipse_height = height;
+        this.ellipse_width = width;
         this.angle = angle;
     }
 
@@ -49,11 +36,24 @@ public class EllipseOverlay implements CameraMapOverlay, Serializable {
 
     @Override
     public void paint(Graphics2D g2, FactHexMapDisplay map) {
+        double radius = map.getTileRadiusInPixels();
         g2.setPaint(fillColor);
         g2.setStroke(new BasicStroke(2));
         AffineTransform old = g2.getTransform();
-        g2.rotate(angle, ellipseCenterX, ellipseCenterY);
+
+        double scalingX = 0.172*radius;
+        double scalingY = 0.184*radius;
+
+        Ellipse2D el = new Ellipse2D.Double( - 0.5 * ellipse_height * scalingX, - 0.5 * ellipse_width * scalingY,
+                this.ellipse_height *scalingX, this.ellipse_width * scalingY);
+
+        double centerX = cogX*scalingX;
+        double centerY = -cogY*scalingY ;
+        g2.translate(centerX, centerY);
+        g2.rotate(-angle);
+
         g2.draw(el);
+
         g2.setTransform(old);
     }
 }
