217bb8fe63fec1d750c599652bfb6b80a44f9d41
Kai Brügge <kai.bruegge@edu.udo>
2014-08-12 15:31:07 +0200

    refactor firfilter

--- src/main/java/fact/filter/FirFilter.java
+++ src/main/java/fact/filter/FirFilter.java
@@ -4,9 +4,11 @@
 package fact.filter;
 
 import fact.Constants;
-import fact.utils.SimpleFactEventProcessor;
+import fact.Utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import stream.Data;
+import stream.Processor;
 import stream.annotations.Description;
 import stream.annotations.Parameter;
 
@@ -20,67 +22,54 @@ import stream.annotations.Parameter;
  * 
  */
 @Description(group = "Fact Tools.Filter", text = "")
-public class FirFilter extends SimpleFactEventProcessor<double[], double[]>{
-	static Logger log = LoggerFactory.getLogger(FirFilter.class);
-	double[] coefficients = { 0.5f, 0.2f, 0.1f };
-	
-	// templates for filter types. will set the coefficents array accordingly.  this is just for user convenience
-	private String template = "cfd";
-	
+public class FirFilter implements Processor {
+    static Logger log = LoggerFactory.getLogger(FirFilter.class);
 
-	@Override
-	public double[] processSeries(double[] data) {
-		double[] result = new double[data.length];
+    @Parameter(required = false, description = "Filter coefficents array. {n, n-1, n-2, ..}.", defaultValue = "{0.5f,0.2f, 0.1f}")
+    double[] coefficients = {0.5f, 0.2f, 0.1f};
 
-		// foreach pixel
-		int roi = data.length / Constants.NUMBEROFPIXEL;
-		for (int pix = 0; pix < Constants.NUMBEROFPIXEL; pix++) {
-			// result[pix*roi] =
-			// iterate over all slices
-			for (int slice = 0; slice < roi; slice++) {
-				int pos = pix * roi + slice;
+    @Parameter(required = true)
+    private String key;
 
-				for (int i = Math.min(slice, coefficients.length - 1); i >= 0; i--) {
-					// System.out.println("i: " + i);
-					result[pos] += coefficients[i] * data[pos - i];
-				}
-			}
-		}
-		return result;
-	}	
+    @Parameter(required = true)
+    private String outputKey;
 
 
-	/* Getter and Setter*/
+    @Override
+    public Data process(Data input) {
+        Utils.isKeyValid(input, key, double[].class);
+        double[] data = (double[]) input.get(key);
+        double[] result = new double[data.length];
 
-	public double[] getCoefficents() {
-		return coefficients;
-	}
+        // foreach pixel
+        int roi = data.length / Constants.NUMBEROFPIXEL;
+        for (int pix = 0; pix < Constants.NUMBEROFPIXEL; pix++) {
+            // result[pix*roi] =
+            // iterate over all slices
+            for (int slice = 0; slice < roi; slice++) {
+                int pos = pix * roi + slice;
 
-	@Parameter(required = true, description = "Filter coefficents array. {n, n-1, n-2, ..}.", defaultValue = "{0.5f,0.2f, 0.1f}")
-	public void setCoefficents(double[] coefficents) {
-		this.coefficients = coefficents;
-	}
-	
-	public String getTemplate() {
-		return template;
-	}
+                for (int i = Math.min(slice, coefficients.length - 1); i >= 0; i--) {
+                    // System.out.println("i: " + i);
+                    result[pos] += coefficients[i] * data[pos - i];
+                }
+            }
+        }
+        input.put(outputKey, result);
+        return input;
 
-	@Parameter(required = false, description = "Template strings for the coefficents array.", defaultValue = "cfd")
-	public void setTemplate(String templateString) {
-		this.template = templateString.toLowerCase();
-		if (template.equals("cfd")) {
-			setCoefficents(Constants.COEFFICENTS_CFD);
-		} else if (template.equals("n3_average")) {
-			setCoefficents(Constants.COEFFICENTS_N3);
-		} else if (template.equals("n5_average")) {
-			setCoefficents(Constants.COEFFICENTS_N5);
-		} else if (template.equals("removesignal")) {
-			setCoefficents(Constants.COEFFICENTS_REMOVE_SIGNAL);
-		} else {
-			log.info("The String "
-					+ template
-					+ " doesn't match any known template. Possible templates are: n3_average, n5_average, removeSignal, cfd ");
-		}
+    }
 
-	}
+    public void setCoefficients(double[] coefficients) {
+        this.coefficients = coefficients;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public void setOutputKey(String outputKey) {
+        this.outputKey = outputKey;
+    }
+
 }
