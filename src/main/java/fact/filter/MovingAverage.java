9632be1555835d21f28a9605dede6651ea9232ba
Kai Brügge <kai.bruegge@edu.udo>
2014-08-12 15:31:36 +0200

    refactor and simplified moving average

--- src/main/java/fact/filter/MovingAverage.java
+++ src/main/java/fact/filter/MovingAverage.java
@@ -1,12 +1,15 @@
 package fact.filter;
 
+import fact.Utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import stream.Data;
+import stream.ProcessContext;
+import stream.StatefulProcessor;
 import stream.annotations.Description;
-//import stream.annotations.Parameter;
 import fact.Constants;
-import fact.utils.SimpleFactEventProcessor;
+import stream.annotations.Parameter;
 
 /**
  * 
@@ -14,79 +17,84 @@ import fact.utils.SimpleFactEventProcessor;
  * 
  */
 @Description(group = "Fact Tools.Filter", text = "A simple running average")
-public class MovingAverage extends SimpleFactEventProcessor<double[],  double[]> {
-	
+public class MovingAverage implements StatefulProcessor{
+
 	static Logger log = LoggerFactory.getLogger(MovingAverage.class);
 
 
+    @Parameter(required = true)
+    private String key;
+
+    @Parameter(required = true)
+    private String outputKey;
+
+    @Parameter(required = true)
 	private int length = 5;
 
-	@Override
-	public double[] processSeries(double[] data) {
-		if(length%2 == 0){
-			length++;
-			log.info("CentralMovingAverage only supports uneven window lengths. New length is: " + length);
-		}
-		int pivot = (int) (length/2.0);
-		double[] result;
-		result = new double[data.length];
-		
-		int roi = data.length / Constants.NUMBEROFPIXEL;
+    @Override
+    public Data process(Data input) {
+        Utils.isKeyValid(input, key, double[].class);
+        double[] data = (double[]) input.get(key);
+        double[] result = new double[data.length];
 
-		for(int pix = 0; pix < Constants.NUMBEROFPIXEL; pix++) {
-			double sum = 0;
-			for(int slice = 0; slice <= pivot; slice++){
-				int pos = pix * roi + slice;
-					
-				for(int slice1 = 0; slice1 <= slice+pivot; slice1++){
-						int pos1 = pix*roi + slice1;
-						sum += data[pos1];
-					}
-				
-				double average = sum/(pivot+slice);
-				result[pos]=average;
-			}
-			
-			for(int slice = pivot; slice <= (roi - 1) - pivot; slice++){
-				sum = 0;
-				int pos = pix * roi + slice;
-				int start = slice - pivot;
-				int end = slice + pivot;
-				
-				for(int i = start; i <= end; i++){
-					int pos1 = pix*roi + i;
-					sum += data[pos1];
-				}		
-				
-				double average = sum/length;
-				result[pos]=average;
-			}
-					
-			for(int slice = roi - pivot; slice < roi; slice++){
-				sum = 0;
-				int pos = pix*roi + slice;
-				
-				for(int slice1 = roi - pivot; slice1 < roi; slice1++){
-					int pos1 = pix*roi + slice1;
-					sum += data[pos1];
-				}
-				
-				double average = sum/(roi-slice);
-				result[pos]=average;
-			}
-		
-		}
-		return result;
-	}
-		
-	
-	/* Getter and Setter*/
+        int roi = data.length / Constants.NUMBEROFPIXEL;
 
-	public int getLength() {
-		return length;
-	}
-	public void setLength(int length) {
-		this.length = length;
-	}
+        for(int pix = 0; pix < Constants.NUMBEROFPIXEL; pix++) {
+            for(int pivot = 0; pivot < roi; pivot++){
+                int pivotPosition = pix*roi + pivot;
+                int seriesEnd = pix*roi + roi;
+                int seriesStart = pix*roi;
+                int count = 0;
+                //loop over the window
+                //intentional precission loss by division
+                int start = pivotPosition - length/2;
+                int end = pivotPosition + length/2 + 1;
 
+                start = start > seriesStart ? start : seriesStart;
+                end = end < seriesEnd ? end : seriesEnd;
+
+                double sum = 0;
+                for(int i = start; i < end; i++){
+//                    int pos = pix*roi + i;
+                    if (i !=  pivotPosition) {
+                        sum += data[i];
+                        count++;
+                    }
+                }
+                result[pivotPosition] = sum/count;
+            }
+        }
+        input.put(outputKey, result);
+        return input;
+    }
+
+    @Override
+    public void init(ProcessContext context) throws Exception {
+        if((length & 1) == 0){
+            length++;
+            log.info("CentralMovingAverage only supports uneven window lengths. New length is: " + length);
+        }
+    }
+
+    @Override
+    public void resetState() throws Exception {
+
+    }
+
+    @Override
+    public void finish() throws Exception {
+
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public void setOutputKey(String outputKey) {
+        this.outputKey = outputKey;
+    }
 }
