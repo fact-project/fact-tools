15b55691916eeee16ba9f14f165f449568234c7d
Kai Brügge <kai.bruegge@edu.udo>
2014-08-15 12:25:48 +0200

    eventinfo panel now shows actually useful information

--- src/main/java/fact/hexmap/ui/components/EventInfoPanel.java
+++ src/main/java/fact/hexmap/ui/components/EventInfoPanel.java
@@ -14,12 +14,14 @@ import fact.hexmap.FactCameraPixel;
 import fact.hexmap.ui.Bus;
 import fact.hexmap.ui.EventObserver;
 import fact.hexmap.ui.PixelSelectionObserver;
+import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
 import org.apache.commons.math3.util.Pair;
 import stream.Data;
 
 import javax.swing.*;
 import java.awt.*;
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -34,21 +36,40 @@ public class EventInfoPanel extends JPanel implements EventObserver, PixelSelect
     final private String disabledString = "No Data in Event";
 	/** The unique class ID */
 	private static final long serialVersionUID = -4439223773970111981L;
-	JLabel eventNumber = new JLabel("EventNumber: ");
+    JLabel eventNumber = new JLabel("EventNumber: ");
     JTextField roiField = new JTextField(disabledString);
     JTextField timeField = new JTextField(disabledString);
     JTextField runIDField = new JTextField(disabledString);
+    JTextField chargeField = new JTextField(disabledString);
+    JTextField chargeFieldStd = new JTextField(disabledString);
+    JTextField sizeField = new JTextField(disabledString);
+
+    JTextField widthField = new JTextField(disabledString);
+    JTextField lengthField = new JTextField(disabledString);
+
+
     DefaultListModel<String> model = new DefaultListModel<>();
     JList<String> pixIDList = new JList<>(model);
+    private DescriptiveStatistics eventStatistics;
+    private Set<CameraPixel> selectedPixel = new HashSet<>();
+    private double[] photonChargeArray;
 
 
     @Override
     @Subscribe
     public void handlePixelSelectionChange(Set<CameraPixel> selectedPixel) {
+        this.selectedPixel = selectedPixel;
+        updateSelectionInfo(selectedPixel);
+    }
+
+    private void updateSelectionInfo(Set<CameraPixel> selectedPixel) {
         model.clear();
         for (CameraPixel sp : selectedPixel){
             FactCameraPixel p = (FactCameraPixel) sp;
-            String m = "Chid: " + p.id + "  SoftId: " + p.softid  + "    Coordinates: " + p.getXPositionInMM() + ", " + p.getYPositionInMM();
+            String m = "Chid: " + p.id + "  SoftId: " + p.softid;
+            if(photonChargeArray !=  null){
+                m += "   photonCharge: " + photonChargeArray[p.id];
+            }
             model.addElement(m);
         }
     }
@@ -56,8 +77,11 @@ public class EventInfoPanel extends JPanel implements EventObserver, PixelSelect
     @Override
     @Subscribe
     public void handleEventChange(Pair<Data, String> itemKeyPair) {
+
+        //get the current event
         Data item = itemKeyPair.getFirst();
-        //System.out.println(item.toString());
+
+        //set the fields if theres data in the event
         Integer roi = (Integer)item.get("NROI");
         if(roi != null){
             roiField.setText(roi.toString());
@@ -78,6 +102,67 @@ public class EventInfoPanel extends JPanel implements EventObserver, PixelSelect
             runIDField.setText(runid.toString());
             runIDField.setEnabled(true);
         }
+        //get photoncharge if its in the map
+        Serializable chargeArray = item.get("@photoncharge");
+        if (chargeArray != null){
+            try{
+                photonChargeArray = ((double[]) chargeArray);
+                eventStatistics = new DescriptiveStatistics();
+                for(double v : photonChargeArray){
+                    eventStatistics.addValue(v);
+                }
+                chargeField.setText("" + eventStatistics.getMean());
+                chargeField.setEnabled(true);
+
+                chargeFieldStd.setText("" + eventStatistics.getStandardDeviation());
+                chargeFieldStd.setEnabled(true);
+            } catch (ClassCastException e){
+                //pass
+            }
+        }
+
+        //get size if its in the map
+        if (item.get("@size") != null){
+            try{
+                double size = (double) item.get("@size");
+                sizeField.setText("" + size);
+                sizeField.setEnabled(true);
+            } catch (ClassCastException e){
+                //pass
+            }
+        } else {
+            sizeField.setText(disabledString);
+            sizeField.setEnabled(false);
+        }
+
+        //get width and length if they are in the map
+        if (item.get("@width") != null && item.get("@length") != null){
+            try{
+                double width = (double) item.get("@width");
+                double length = (double) item.get("@length");
+                widthField.setText("" + width );
+                widthField.setEnabled(true);
+
+                lengthField.setText("" + length );
+                lengthField.setEnabled(true);
+            } catch (ClassCastException e){
+                //pass
+            }
+        } else {
+            widthField.setText(disabledString);
+            widthField.setEnabled(false);
+
+            lengthField.setText(disabledString);
+            lengthField.setEnabled(false);
+        }
+
+        if(runid !=  null){
+            runIDField.setText(runid.toString());
+            runIDField.setEnabled(true);
+        }
+
+        //update the selection info now that we have the photoncharge
+        updateSelectionInfo(this.selectedPixel);
     }
 
 
@@ -93,28 +178,40 @@ public class EventInfoPanel extends JPanel implements EventObserver, PixelSelect
         roiField.setEditable(false);
         timeField.setEditable(false);
         runIDField.setEditable(false);
+        chargeField.setEditable(false);
+        chargeFieldStd.setEditable(false);
+        sizeField.setEditable(false);
+        widthField.setEditable(false);
+        lengthField.setEditable(false);
 
         roiField.setEnabled(false);
         runIDField.setEnabled(false);
         timeField.setEnabled(false);
-
+        chargeField.setEnabled(false);
+        chargeFieldStd.setEnabled(false);
+        sizeField.setEnabled(false);
+        widthField.setEnabled(false);
+        lengthField.setEnabled(false);
         // set layout of the main window
         FormLayout layout = new FormLayout(new ColumnSpec[] {
-                ColumnSpec.decode("left:pref:grow"),
+                ColumnSpec.decode("left:100px:grow"),
                 ColumnSpec.decode("pref"),
-                //apparently setting this to 100px does what i want. I have no idea why
+
                 ColumnSpec.decode("fill:100px:grow"),
                 ColumnSpec.decode("pref"),
 
                 ColumnSpec.decode("right:pref:grow"),
                 ColumnSpec.decode("pref"),
-
         },
                 new RowSpec[] {
-                        RowSpec.decode("fill:12dlu:grow"),
+                        RowSpec.decode("fill:12dlu:grow"),//roi and stuff
                         RowSpec.decode("fill:pref:grow"),
-                        RowSpec.decode("fill:12dlu:grow"),
+                        RowSpec.decode("fill:12dlu:grow"),//selection info
                         RowSpec.decode("top:pref"),
+                        RowSpec.decode("fill:12dlu:grow"), //physics
+                        RowSpec.decode("fill:pref"),
+                        RowSpec.decode("fill:pref"),
+                        RowSpec.decode("fill:pref")
                 });
 
         PanelBuilder builder = new PanelBuilder(layout);
@@ -132,17 +229,29 @@ public class EventInfoPanel extends JPanel implements EventObserver, PixelSelect
         builder.add(new JLabel(("Pixel IDs")), cc.xy(1,4));
         builder.add(new JScrollPane(pixIDList), cc.xywh(2,4,5, 1));
 
+        builder.addSeparator("Addition Physics Information", cc.xywh(1,5,6,1));
+        builder.add(new JLabel("Charge Mean/Deviation"), cc.xywh(1,6, 2, 1));
+        builder.add(chargeField, cc.xywh(3, 6, 1, 1));
+        builder.add(chargeFieldStd, cc.xywh(4, 6, 1, 1));
+        builder.add(new JLabel("Width, Length"), cc.xywh(1,7, 2, 1));
+        builder.add(widthField, cc.xywh(3, 7, 1, 1));
+        builder.add(lengthField, cc.xywh(4, 7, 1, 1));
+
+        builder.add(new JLabel("Size (of Shower)"), cc.xywh(1,8, 2, 1));
+        builder.add(sizeField, cc.xywh(3, 8, 2, 1));
+
+
+
         contentPanel.add(builder.getPanel());
         add(contentPanel);
-
 	}
 
 
     public static void main(String[]args){
         JFrame frame = new JFrame();
-        frame.setSize(400, 300);
+        frame.setSize(600, 400);
 
-        EventInfoPanel n = new EventInfoPanel(400,300);
+        EventInfoPanel n = new EventInfoPanel(600,400);
 
         frame.getContentPane().add(n);
         frame.pack();
