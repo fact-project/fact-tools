0418346c5c1e10708da2590ed82cd50a993b62b6
Fabian Temme <fabian.temme@tu-dortmund.de>
2014-08-21 15:33:52 +0200

    Removed obsolet processores, moved PixelDistribution2D to the container package

--- src/main/java/fact/features/HillasWidth.java
+++ src/main/java/fact/features/HillasWidth.java
@@ -1,9 +1,11 @@
 package fact.features;
 
 import fact.Utils;
-import fact.statistics.PixelDistribution2D;
+import fact.container.PixelDistribution2D;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import stream.Data;
 import stream.Processor;
 import stream.annotations.Parameter;
