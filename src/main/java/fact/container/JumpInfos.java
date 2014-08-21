7325be38abe70c0ac900649aa8cc112aa8c947d1
Fabian Temme <fabian.temme@tu-dortmund.de>
2014-08-21 14:23:10 +0200

    Finishing the refactoring of patchjumpremoval

--- src/main/java/fact/container/JumpInfos.java
+++ src/main/java/fact/container/JumpInfos.java
@@ -4,37 +4,86 @@ import java.io.Serializable;
 
 import org.jfree.chart.plot.IntervalMarker;
 
+import stream.Data;
 import fact.hexmap.ui.overlays.PixelSetOverlay;
 
 public class JumpInfos implements Serializable {
 
 	private static final long serialVersionUID = -4430692866625609266L;
-    public PixelSetOverlay pixelWithSpikes;
-    public PixelSetOverlay pixelWithSignalFlanks;
-    public PixelSetOverlay pixelWithRinging;
-    public PixelSetOverlay pixelWithCorrectedJumps;
-    public PixelSetOverlay pixelWithWrongTimeDepend;
 	
-    public double[] averJumpHeights = null;
+    private PixelSetOverlay pixelWithSpikes;
+    private PixelSetOverlay pixelWithSignalFlanks;
+    private PixelSetOverlay pixelWithRinging;
+    private PixelSetOverlay pixelWithCorrectedJumps;
+    private PixelSetOverlay pixelWithWrongTimeDepend;
 	
-    public double[] fftResults = null;
+    public double[] averJumpHeights;
+	
+    public double[] fftResults;
 	
     public IntervalMarker[] posMarker;
 
+	public JumpInfos(int numberOfPixel, int numberOfPatches, int roi) {
+		averJumpHeights = new double[numberOfPatches];
+		posMarker = new IntervalMarker[numberOfPixel];
+		fftResults = new double[numberOfPixel*roi];
+	}
+	
+	public void addPixelWithSpikes(int pixel){
+		pixelWithSpikes.addById(pixel);
+	}
+	
+	public void addPatchWithSignalFlanks(int patch){
+		for (int px = 0 ; px < 9 ; px++)
+		{
+			pixelWithSignalFlanks.addById(patch*9+px);
+		}
+	}
+	
+	public void addPatchWithRinging(int patch){
+		for (int px = 0 ; px < 9 ; px++)
+		{
+			pixelWithRinging.addById(patch*9+px);
+		}
+	}
+	
+	public void addPatchWithCorrectedJumps(int patch){
+		for (int px = 0 ; px < 9 ; px++)
+		{
+			pixelWithCorrectedJumps.addById(patch*9+px);
+		}
+	}
+	
+	public void addPatchWithWrongTiming(int patch){
+		for (int px = 0 ; px < 9 ; px++)
+		{
+			pixelWithWrongTimeDepend.addById(patch*9+px);
+		}
+	}
+	
+	public void addInfosToDataItem(Data input,int prevEvent, String name, double deltaT)
+	{
+		input.put(name+prevEvent+"Jumps", averJumpHeights);
+		input.put(name+prevEvent+"Time", deltaT);
+		input.put(name+prevEvent+"Spikes", pixelWithSpikes);
+		input.put(name+prevEvent+"SignalFlanks", pixelWithSignalFlanks);
+		input.put(name+prevEvent+"Ringing", pixelWithRinging);
+		input.put(name+prevEvent+"JumpsSet", pixelWithCorrectedJumps);
+		input.put(name+prevEvent+"TimeSet", pixelWithWrongTimeDepend);
+		input.put(name+prevEvent+"Marker", posMarker);
+		input.put(name+prevEvent+"fftResults", fftResults);
+	}
+
+	public void addPosMarkerForPatch(int patch, short pos) {
+		for (int px = 0 ; px < 9 ; px++)
+		{
+			posMarker[patch*9+px] = new IntervalMarker(pos, pos+1);
+		}
+	}
+    
+    
+
 //    
-//	input.put(outputJumpsKey+prevEvent+"Jumps", averJumpHeights);
-//	input.put(outputJumpsKey+prevEvent+"Time", deltaT);
-//////	input.put(outputJumpsKey+prevEvent+"Spikes", pixelWithSpikes);
-//////	input.put(outputJumpsKey+prevEvent+"SignalFlanks", pixelWithSignalFlanks);
-//////	input.put(outputJumpsKey+prevEvent+"Ringing", pixelWithRinging);
-////	input.put(outputJumpsKey+prevEvent+"JumpsSet", pixelWithCorrectedJumps);
-////	if (pixelWithWrongTimeDepend.size() > 0)
-////	{
-////		input.put(outputJumpsKey+prevEvent+"TimeSet", pixelWithWrongTimeDepend);
-////	}
-////	input.put(outputJumpsKey+prevEvent+"Marker", posMarker);
-////	input.put(outputJumpsKey+prevEvent+"patchAverage", patchAverageCamera);
-////	input.put(outputJumpsKey+prevEvent+"patchAverageDeriv", patchAverageDerivCamera);
-////	input.put(outputJumpsKey+prevEvent+"fftResults", fftResults);
+//	
 ////	input.put("@"+Constants.KEY_COLOR + "_" +outputJumpsKey+prevEvent+"fftResults", "#0ACF1B");
 }
--- src/main/java/fact/datacorrection/PatchJumpRemoval.java
+++ src/main/java/fact/datacorrection/PatchJumpRemoval.java
@@ -90,7 +90,7 @@ public class PatchJumpRemoval implements Processor {
 		int numberPatches = Constants.NUMBEROFPIXEL / 9;
 		
 
-		jumpInfos = new JumpInfos();
+		jumpInfos = new JumpInfos(Constants.NUMBEROFPIXEL, numberPatches, roi);
 				
 		boolean stopLoop = false;
 		
@@ -130,7 +130,13 @@ public class PatchJumpRemoval implements Processor {
 				}
 				if (posInROI == true)
 				{
+					if (addJumpInfos == true)
+					{
+						jumpInfos.addPosMarkerForPatch(patch,pos);
+					}
 					result = HandleSpike(patch, pos, result, jumpInfos);
+					
+					
 					
 					// create patch voltage average array and patch voltage derivation array:
 					double[] patchAverage = new double[roi];
@@ -162,7 +168,10 @@ public class PatchJumpRemoval implements Processor {
 					stopLoop = false;
 				}
 			}
-
+			if (addJumpInfos == true)
+			{
+				jumpInfos.addInfosToDataItem(input, prevEvent, outputJumpsKey, deltaT);
+			}
 		}
 		input.put(outputKey,result);
 		
@@ -195,7 +204,7 @@ public class PatchJumpRemoval implements Processor {
 						result[pixel*roi+pos] = correctionValue;
 						if (addJumpInfos == true)
 						{
-							jumpInfo.pixelWithSpikes.addById(pixel);
+							jumpInfo.addPixelWithSpikes(pixel);
 						}
 					}
 			}
@@ -215,7 +224,7 @@ public class PatchJumpRemoval implements Processor {
 						result[pixel*roi+pos+1] = correctionValue;
 						if (addJumpInfos == true)
 						{
-							jumpInfo.pixelWithSpikes.addById(pixel);
+							jumpInfo.addPixelWithSpikes(pixel);
 						}
 					}
 			}
@@ -233,7 +242,7 @@ public class PatchJumpRemoval implements Processor {
 						result[pixel*roi+pos+2] = correctionValue;
 						if (addJumpInfos == true)
 						{
-							jumpInfo.pixelWithSpikes.addById(pixel);
+							jumpInfo.addPixelWithSpikes(pixel);
 						}
 					}
 			}			
@@ -250,7 +259,7 @@ public class PatchJumpRemoval implements Processor {
 					result[pixel*roi+pos] = correctionValue;
 					if (addJumpInfos == true)
 					{
-						jumpInfo.pixelWithSpikes.addById(pixel);
+						jumpInfo.addPixelWithSpikes(pixel);
 					}
 				}
 			}
@@ -267,7 +276,7 @@ public class PatchJumpRemoval implements Processor {
 					result[pixel*roi+pos+1] = correctionValue;
 					if (addJumpInfos == true)
 					{
-						jumpInfo.pixelWithSpikes.addById(pixel);
+						jumpInfo.addPixelWithSpikes(pixel);
 					}
 				}
 			}
@@ -343,10 +352,7 @@ public class PatchJumpRemoval implements Processor {
 			timeDependIsCorrect = false;
 			if (addJumpInfos == true)
 			{
-				for (int px = 0 ; px < 9 ; px++)
-				{
-					jumpInfos.pixelWithWrongTimeDepend.addById(patch*9+px);
-				}
+				jumpInfos.addPatchWithWrongTiming(patch);
 			}
 			jumpHeight = 0;
 		}
@@ -393,10 +399,7 @@ public class PatchJumpRemoval implements Processor {
 			noSignalFlank = false;
 			if (addJumpInfos == true)
 			{
-				for (int px = 0 ; px < 9 ; px++)
-				{
-					jumpInfos.pixelWithSignalFlanks.addById(patch*9+px);
-				}
+				jumpInfos.addPatchWithSignalFlanks(patch);
 			}
 			jumpHeight = 0;
 		}
@@ -502,10 +505,7 @@ public class PatchJumpRemoval implements Processor {
 			jumpHeight -= averRingingDerviation;
 			if (addJumpInfos == true)
 			{
-				for (int px = 0 ; px < 9 ; px++)
-				{
-					jumpInfos.pixelWithRinging.addById(patch*9+px);
-				}
+				jumpInfos.addPatchWithRinging(patch);
 			}
 
 			if (Math.abs(jumpHeight) < jumpLimit)
@@ -535,10 +535,8 @@ public class PatchJumpRemoval implements Processor {
 		
 		if (addJumpInfos == true)
 		{
-			for (int px = 0 ; px < 9 ; px++)
-			{
-				jumpInfos.pixelWithCorrectedJumps.addById(patch*9+px);
-			}
+			jumpInfos.addPatchWithCorrectedJumps(patch);
+			jumpInfos.averJumpHeights[patch] = jumpHeight;
 		}
 		
 		if (isStartCell == false)
