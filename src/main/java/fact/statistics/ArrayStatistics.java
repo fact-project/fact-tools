e980da138ad3ed39a8dfacf4723a1905a68fe52b
Kai Brügge <kai.bruegge@edu.udo>
2014-10-09 15:04:21 +0200

    rewrote the array statistics stuff

--- src/main/java/fact/statistics/ArrayStatistics.java
+++ src/main/java/fact/statistics/ArrayStatistics.java
@@ -1,74 +1,54 @@
 package fact.statistics;
 
 import fact.Utils;
+import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
+import org.apache.commons.math3.stat.descriptive.SummaryStatistics;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import stream.Data;
 import stream.Processor;
+import stream.annotations.Parameter;
 
+/**
+ * Writes some information about the given array to the data item. The following information will be written:
+ *
+ * outputKey+"_" +"mean"
+ * outputKey+"_" +"max"
+ * outputKey+"_" +"min"
+ * outputKey+"_" +"geommetricMean"
+ * outputKey+"_" +"kurtosis"
+ * outputKey+"_" +"variance"
+ */
 public class ArrayStatistics implements Processor {
 	static Logger log = LoggerFactory.getLogger(ArrayStatistics.class);
+    @Parameter(required = true, description = "Key to the array you want the information about")
 	private String key = null;
+    @Parameter(required = true, description = "The name of the data written to the stream")
 	private String outputKey = null;
 
 	@Override
 	public Data process(Data input) {
-		// TODO Auto-generated method stub
 		Utils.mapContainsKeys( input, key);
 		
 		double[] data = Utils.toDoubleArray(input.get(key));
-		
-		int n = data.length;
-		
-		double max = 0;
-		int max_pos = 0;
-		double min = Double.MAX_VALUE;
-		int min_pos = 0;
-		double mean = 0;
-		double rms = 0;
-		double values = 0;
-		double valuesSquare = 0;
-		
-		for (int i = 0 ; i < n ; i++)
-		{
-			double value = data[i];
-			if (value > max)
-			{
-				max = value;
-				max_pos = i;
-			}
-			if (value < min)
-			{
-				min = value;
-				min_pos = i;
-			}
-			values += value;
-			valuesSquare += value*value;
-		}
-		mean = values / n;
-		rms = Math.sqrt( (valuesSquare - (values*values)/n)/(n-1) );
-		
-		input.put(outputKey+"Mean",mean);
-		input.put(outputKey+"Min",min);
-		input.put(outputKey+"MinPos",min_pos);
-		input.put(outputKey+"Max",max);
-		input.put(outputKey+"MaxPos",max_pos);
-		input.put(outputKey+"Rms",rms);
-		
-		
-		return input;
-	}
 
-	public String getKey() {
-		return key;
+        DescriptiveStatistics s = new DescriptiveStatistics();
+		for(double value: data){
+            s.addValue(value);
+        }
+		input.put(outputKey+"_" +"mean",s.getMean());
+        input.put(outputKey+"_" +"max",s.getMax());
+        input.put(outputKey+"_" +"min",s.getMin());
+        input.put(outputKey+"_" +"geommetricMean",s.getGeometricMean());
+        input.put(outputKey+"_" +"kurtosis",s.getKurtosis());
+        input.put(outputKey+"_" +"variance",s.getVariance());
+
+
+        return input;
 	}
 
 	public void setKey(String key) {
 		this.key = key;
-	}
-
-	public String getOutputKey() {
-		return outputKey;
 	}
 
 	public void setOutputKey(String outputKey) {
