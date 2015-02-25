248c704575565388273aa6f24bff691031f20445
Dominik Neise <dominik.neise@udo.edu>
2015-02-25 12:46:16 +0100

    using class resource in test. added some comments to the test and explicitely called SetDataKey.

--- src/test/java/fact/datacorrection/DrsTimeCalibrationTest.java
+++ src/test/java/fact/datacorrection/DrsTimeCalibrationTest.java
@@ -17,9 +17,10 @@ public class DrsTimeCalibrationTest {
     public void loadFileTest() {
         try {
             DrsTimeCalibration drsTimeCalibration = new DrsTimeCalibration();
-            URL resource = DrsTimeCalibrationTest.class.getResource("/testDrsTimeFile.time.drs.fits");
+            URL resource = DrsTimeCalibration.class.getResource("/long_term_constants_median.time.drs.fits");
 
             drsTimeCalibration.setUrl(new SourceURL(resource));
+            drsTimeCalibration.setDataKey("CellOffset");
             drsTimeCalibration.init(null);
 
         } catch (Exception e) {
@@ -32,20 +33,31 @@ public class DrsTimeCalibrationTest {
     public void constantsCalibrationSanity()  {
         try {
             DrsTimeCalibration drsTimeCalibration = new DrsTimeCalibration();
-            URL resource = DrsTimeCalibrationTest.class.getResource("/testDrsTimeFile.time.drs.fits");
+            URL resource = DrsTimeCalibration.class.getResource("/long_term_constants_median.time.drs.fits");
 
             drsTimeCalibration.setUrl(new SourceURL(resource));
+            drsTimeCalibration.setDataKey("CellOffset");
             drsTimeCalibration.init(null);
 
 
-            //these constant
-            double[][] constants = drsTimeCalibration.samplingConstants;
-            Assert.assertNotNull(constants);
+            //this true_sampling_time array contains the most precise time of sampling
+            // for each pixel. It contains 2048 values for each pixel, in order to
+            // make accessing them easier.
+            // One would usually access true_sampling_time[the_current_drs_chip_id][startcell:startcell+roi]
+            // in order to work on pixel data.
+            double[][] true_sampling_time = drsTimeCalibration.true_sampling_time;
+            Assert.assertNotNull(true_sampling_time);
+
+            // the true_sampling_time is measured in 'nominal_time_slices'. This means:
+            // In a perfect world the true_sampling_time is identical with the sample index.
+            // But in a real world the true_samplig_time slightly differs from the sample_index.
+            // We have never seen them deviate by more than +-7 nominal slices.
+            // So I simply assert here that they deviate by not more than 10.
 
             for(int patch = 0; patch < 160; patch++){
-                for (int t = 0; t < constants.length; t++) {
-                    Assert.assertTrue(constants[patch][t] < 2060);
-                    Assert.assertTrue(constants[patch][t] > -10);
+                for (int t = 0; t < true_sampling_time.length; t++) {
+                    Assert.assertTrue(true_sampling_time[patch][t]-t <= 10);
+                    Assert.assertTrue(true_sampling_time[patch][t]-t > -10);
                 }
             }
         } catch (Exception e) {
