9b1a6656eb42ad9d6a2fbf52e3ef581b9270b282
Kai <kai.bruegge@gmail.com>
2015-02-28 20:16:16 +0100

    Hopefully less annoying output fo zfits reader.

--- src/test/java/fact/FunctionalTest.java
+++ src/test/java/fact/FunctionalTest.java
@@ -14,6 +14,7 @@ import java.util.Arrays;
 import static org.junit.Assert.fail;
 
 /**
+ * Test some of the provided example xmls in the examples folder. This wont cause failing builds just yet.
  * Created by kai on 28.02.15.
  */
 public class FunctionalTest {
