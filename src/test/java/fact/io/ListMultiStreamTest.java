b0969950b7b152c26ac9ddf9012b7a1f1a816b36
Kai Brügge <kai.bruegge@udo.edu>
2015-04-28 16:06:01 +0200

    added first version of a  documentation for multistreams and repaired some tests

--- src/test/java/fact/io/ListMultiStreamTest.java
+++ src/test/java/fact/io/ListMultiStreamTest.java
@@ -10,6 +10,7 @@ import java.net.URL;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
 
 /**
  * Created by kaibrugge on 14.04.15.
@@ -18,10 +19,28 @@ public class ListMultiStreamTest {
     static Logger log = LoggerFactory.getLogger(ListMultiStreamTest.class);
 
     @Test
-    public void readJsonListTest() throws Exception {
+    public void readBrokenJsonTest() throws Exception {
+        URL u =  ListMultiStreamTest.class.getResource("/dummy_files/file_drs_list.json");
         FactFileListMultiStream multiStream = new FactFileListMultiStream();
-        URL u =  FitsStreamTest.class.getResource("/dummy_files/file_drs_list.json");
         multiStream.setListUrl(new SourceURL(u));
+
+        try {
+            multiStream.setDrsPathKey("bla");
+            multiStream.init();
+        } catch (IllegalArgumentException e){
+            return;
+        }
+
+        fail("This shoudl have thrown an illegalargumentexception");
+    }
+
+    @Test
+    public void readJsonTest() throws Exception {
+        URL u =  ListMultiStreamTest.class.getResource("/dummy_files/file_drs_list.json");
+        FactFileListMultiStream multiStream = new FactFileListMultiStream();
+        multiStream.setListUrl(new SourceURL(u));
+
+        multiStream.setDrsPathKey("closest");
 
         assertThat(multiStream.fileQueue.size(), is(0));
         multiStream.init();
