e97eea6be5999f09eca85358782b409f5f0c9d79
Kai Bruegge <kai.bruegge@udo.edu>
2015-07-15 15:45:49 +0200

    added tutorial processors

--- src/main/java/fact/tutorial/Cleaning.java
+++ src/main/java/fact/tutorial/Cleaning.java
@@ -1,7 +1,36 @@
 package fact.tutorial;
 
+import fact.hexmap.ui.overlays.PixelSetOverlay;
+import stream.Data;
+import stream.Processor;
+import stream.annotations.Parameter;
+
+import java.util.HashSet;
+
 /**
  * Created by kai on 15.07.15.
  */
-public class Cleaning {
+public class Cleaning implements Processor {
+
+    @Parameter(required = false, description = "The threshold for selecting pixels. Every pixel with a value > threshold" +
+            "will be selected")
+    private double threshold = 15.0;
+
+
+    @Override
+    public Data process(Data item) {
+
+        double[] photons = (double[]) item.get("photons");
+        HashSet<Integer> selectedPixel = new HashSet<>();
+
+        for (int pixel = 0; pixel < 1440; pixel++) {
+            if(photons[pixel] > threshold){
+                selectedPixel.add(pixel);
+            }
+        }
+
+        item.put("showerPixel", selectedPixel);
+        item.put("@overlay", new PixelSetOverlay(selectedPixel));
+        return item;
+    }
 }
--- src/main/java/fact/tutorial/Size.java
+++ src/main/java/fact/tutorial/Size.java
@@ -1,7 +1,23 @@
 package fact.tutorial;
 
+import stream.Data;
+import stream.Processor;
+
 /**
  * Created by kai on 15.07.15.
  */
-public class Size {
+public class Size implements Processor {
+    @Override
+    public Data process(Data data) {
+        double size = 0;
+        int[] showerPixel = (int[]) data.get("showerPixel");
+        double[] photons = (double[]) data.get("photons");
+
+        for (Integer pixel: showerPixel){
+            size += photons[pixel];
+        }
+
+        data.put("size", size);
+        return data;
+    }
 }
