be087674e2789178c21a565f393466d15d71f2ef
Kai Bruegge <kai.bruegge@udo.edu>
2015-08-10 14:46:21 +0200

    Refactor LTC and add another test case

--- src/main/java/fact/utils/LinearTimeCorrectionKernel.java
+++ src/main/java/fact/utils/LinearTimeCorrectionKernel.java
@@ -26,44 +26,35 @@ public class LinearTimeCorrectionKernel implements TimeCorrectionKernel {
 	@Override
 	public double interpolate(double t) {
 		
-		int id = getIndex(t);
-		if (id < numPoints - 1)
-		{
-			// check left border
-			if(id == 0 && times[0] > t)
-				return values[0];
-			
-			double t0 = times[id];
-			double t1 = times[id + 1];
-			double s = (t - t0) / (t1 - t0); // interpolation in "percent"
-		
-			double v0 = values[id];
-			double v1 = values[id + 1];
-			
-			return (v1 - v0) * s + v0;
-		}else // check right border
-		{
-			return values[numPoints - 1];
-		}
-		
-	}
-	
-	/**
-	 * This function return the id of the entry with smaller t. Since the values in the times array
-     * are sorted in ascending order we can use a binary search to find the right index in the array.
-	 * @param t the value to get the index for.
-	 * @return the index of the times array.
-	 */
-	private int getIndex(double t) {
 		int pos = Arrays.binarySearch(times, t);
-		if (pos >= 0){
-			return pos;
-		}else {
-			return - (pos + 1);
-		}
+
+        if (pos >= values.length){
+            return values[values.length - 1];
+        }
+
+        if (pos >= 0){
+            return values[pos];
+        }
+
+        // see semantic of Arrays.binaryseach return code.
+        pos = -(pos +1);
+
+        // in case insertion point is the beginning of the array return the left border
+        if(pos == 0){
+            return values[0];
+        }
+
+
+        double t0 = times[pos -1];
+        double t1 = times[pos];
+
+        // calculate the slope
+        double s = (t - t0) / (t1 - t0);
+
+        double v0 = values[pos -1];
+        double v1 = values[pos];
+
+        return (v1 - v0) * s + v0;
+
 	}
-
-
-
-
 }
