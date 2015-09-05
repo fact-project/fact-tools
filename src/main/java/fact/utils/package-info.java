d7ea41907e8e48d0aa34b25c0f0e68754527390e
Kai Bruegge <kai.bruegge@tu-dortmund.de>
2015-09-05 14:56:18 +0100

    fix some javadoc stuff

--- src/main/java/fact/utils/package-info.java
+++ src/main/java/fact/utils/package-info.java
@@ -1,6 +1,4 @@
 /**
-* <PRE format="md">
-* This package contains processors for common tasks such as Array modifications or Counters. 
-* </PRE>
+* This package contains processors for common tasks such as Array modifications or Counters.
 */
 package fact.utils;
