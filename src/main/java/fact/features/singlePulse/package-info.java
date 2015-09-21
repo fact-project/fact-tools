c0b14985e1ed009bc295343470bb36fda8239291
jebuss <jens.buss@tu-dortmund.de>
2015-09-21 15:34:58 +0100

    added package info

---
+++ src/main/java/fact/features/singlePulse/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * @author Jens Buss
+ *
+ * This packaged contains processors for the analysis of waveforms
+ * with the aim to detect and identify several single photon pulses
+ * on the time of the provided events.
+ *
+ * Created by jbuss on 18.09.15.
+ */
+package fact.features.singlePulse;
