02011ac42ba69a8c49ce35915fd4048ac24b6929
jens buß <jebuss@E5b-Mac-mini-020.local>
2016-01-04 17:52:04 +0100

    return intersection of two pixel sets

--- src/main/java/fact/pixelsets/Intersection.java
+++ src/main/java/fact/pixelsets/Intersection.java
@@ -1,6 +1,7 @@
 package fact.pixelsets;
 
 import com.google.common.collect.Sets;
+import fact.Utils;
 import fact.hexmap.CameraPixel;
 import fact.hexmap.ui.overlays.PixelSetOverlay;
 import org.slf4j.Logger;
@@ -13,36 +14,64 @@ import java.util.Set;
 
 
 /**
- * This processor gets two pixel sets and performs a union of these sets.
- * Created by jebuss on 17.12.15.
+ * This processor gets two pixel sets and performs a intersection of the sets A and B, denoted A ∩ B,
+ * is the set of all objects that are members of both A and B.
+ * The intersection of {1, 2, 3} and {2, 3, 4} is the set {2, 3} .
+ *
+ * Created by jebuss on 04.01.16.
  */
 public class Intersection implements Processor{
     static Logger log = LoggerFactory.getLogger(Intersection.class);
 
-    @Parameter(required = true, description = "key to the first set to be united")
+    @Parameter(required = true, description = "key to the first set to intersect")
     private String inset1Key;
 
-    @Parameter(required = true, description = "key to the second set to be united")
+    @Parameter(required = true, description = "key to the second set to intersect")
     private String inset2Key;
 
-    @Parameter(required = true, description = "key to the output set which contains the union")
+    @Parameter(required = true, description = "key to the output set which contains the intersection")
     private String outsetKey;
-
-    private PixelSetOverlay cleanedPixelSet;
-
+    {}
     @Override
     public Data process(Data input) {
+
+        if (!input.containsKey(inset1Key)) {
+            return input;
+        }
+
+        if (!input.containsKey(inset2Key)) {
+            return input;
+        }
+
+        Utils.isKeyValid(input, inset1Key, PixelSetOverlay.class);
+        Utils.isKeyValid(input, inset2Key, PixelSetOverlay.class);
 
         PixelSetOverlay inset1 = (PixelSetOverlay) input.get(inset1Key);
         PixelSetOverlay inset2 = (PixelSetOverlay) input.get(inset2Key);
 
-        Sets.SetView<CameraPixel> union = Sets.union(inset1.set, inset2.set);
-        Set<CameraPixel> cameraPixels = union.immutableCopy();
+        try{
+            Sets.SetView<CameraPixel> intersection = Sets.intersection(inset1.set, inset2.set);
+            Set<CameraPixel> cameraPixels = intersection.immutableCopy();
 
-        PixelSetOverlay outset = new PixelSetOverlay(cameraPixels);
+            PixelSetOverlay outset = new PixelSetOverlay(cameraPixels);
 
-        input.put(outsetKey, outset);
+            input.put(outsetKey, outset);
+        } catch (NullPointerException e){
+            e.printStackTrace();
+        }
 
         return input;
+    }
+
+    public void setInset1Key(String inset1Key) {
+        this.inset1Key = inset1Key;
+    }
+
+    public void setInset2Key(String inset2Key) {
+        this.inset2Key = inset2Key;
+    }
+
+    public void setOutsetKey(String outsetKey) {
+        this.outsetKey = outsetKey;
     }
 }
