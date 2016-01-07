ccc4acc365e617119bb6cdca1174452957b25d23
Fabian Temme <fabian.temme@tu-dortmund.de>
2016-01-07 09:52:55 +0100

    Moved comments to the processor description

--- src/main/java/fact/calibrationservice/ConstantCalibService.java
+++ src/main/java/fact/calibrationservice/ConstantCalibService.java
@@ -6,9 +6,48 @@ import org.joda.time.DateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import fact.auxservice.AuxFileService;
 import fact.hexmap.FactPixelMapping;
 
+/**
+ * From the beginning on there were
+ * 3 dead pixels:
+ * CHIDs: 927,80,873
+ * 3 crazy pixels:
+ * CHIDs: 863,297,868
+ * 6 twin pixels:
+ * CHIDs: 1093,1094,527,528,721,722
+ * 
+ * All this bad pixels can be interpolated and are all usable for cleaning (so there are no
+ * NotUsablePixels)
+ * 
+ * During the period between 2014/11/15 and 2015/05/26 there was a drs board broken:
+ * SOFTIDs: 1193,1194,1195,1391,1392,1393,1304,1305,1306 
+ * CHIDs:    720, 721, 722, 723, 724, 725, 726, 727, 728 (not the same order as the SOFTIDs!)
+ * The whole patch cannot be interpolated and therefore all pixels are not usable for cleaning.
+ * 
+ * On the 8.1.2015 two more pixels showed no signal and had to be interpolated:
+ * CHIDs: 729,750
+ * 
+ * On the 9.1.2015 only one more pixel showed no signal and had to be interpolated:
+ * CHID: 750
+ * 
+ * On the 31.1.2015 pixel (CHID 750) recovered.
+ * 
+ * With beginning of February 2015 two bias patches got some problems:
+ * The bias patch for the pixels (CHIDs: 171,172,173,174) were dead (so the pixels showed no signal)
+ * between 6.2. and 11.2 (included) and from 16.2. (included) until now
+ * 
+ * There were also some days (12.2. - 14.2.) were the pixels showed two small signals (so maybe 
+ * the bias voltage was too low), but on the other hand on the 15.2. they show normal signals
+ * 
+ * The bias patch for the pixels (CHIDs: 184,185,186,187,188) showed sometimes also a lowered bias voltage
+ * (so signals were smaller) but no clear timewindow could be found
+ * 
+ * Concerning the bad pixel list, the days with only lowered bias voltage are not interpolated, for the days
+ * with no bias voltage the pixels are interpolated.
+ * 
+ * @author ftemme
+ **/
 public class ConstantCalibService implements CalibrationService {
 	
 	Logger log = LoggerFactory.getLogger(ConstantCalibService.class);
@@ -16,48 +55,7 @@ public class ConstantCalibService implements CalibrationService {
 	TreeSet<HardwareConfiguration> set;
 	
 	boolean isInit = false;
-	
-	/*
-	 * From the beginning on there were
-	 * 3 dead pixels:
-	 * CHIDs: 927,80,873
-	 * 3 crazy pixels:
-	 * CHIDs: 863,297,868
-	 * 6 twin pixels:
-	 * CHIDs: 1093,1094,527,528,721,722
-	 * 
-	 * All this bad pixels can be interpolated and are all usable for cleaning (so there are no
-	 * NotUsablePixels)
-	 * 
-	 * During the period between 2014/11/15 and 2015/05/26 there was a drs board broken:
-	 * SOFTIDs: 1193,1194,1195,1391,1392,1393,1304,1305,1306 
-	 * CHIDs:    720, 721, 722, 723, 724, 725, 726, 727, 728 (not the same order as the SOFTIDs!)
-	 * The whole patch cannot be interpolated and therefore all pixels are not usable for cleaning.
-	 * 
-	 * On the 8.1.2015 two more pixels showed no signal and had to be interpolated:
-	 * CHIDs: 729,750
-	 * 
-	 * On the 9.1.2015 only one more pixel showed no signal and had to be interpolated:
-	 * CHID: 750
-	 * 
-	 * On the 31.1.2015 pixel (CHID 750) recovered.
-	 * 
-	 * With beginning of February 2015 two bias patches got some problems:
-	 * The bias patch for the pixels (CHIDs: 171,172,173,174) were dead (so the pixels showed no signal)
-	 * between 6.2. and 11.2 (included) and from 16.2. (included) until now
-	 * 
-	 * There were also some days (12.2. - 14.2.) were the pixels showed two small signals (so maybe 
-	 * the bias voltage was too low), but on the other hand on the 15.2. they show normal signals
-	 * 
-	 * The bias patch for the pixels (CHIDs: 184,185,186,187,188) showed sometimes also a lowered bias voltage
-	 * (so signals were smaller) but no clear timewindow could be found
-	 * 
-	 * Concerning the bad pixel list, the days with only lowered bias voltage are not interpolated, for the days
-	 * with no bias voltage the pixels are interpolated.
-	 * 
-	 */
-	
-	
+		
 	public void init(){
 		FactPixelMapping pixelMap = FactPixelMapping.getInstance();
 		set = new TreeSet<HardwareConfiguration>();
