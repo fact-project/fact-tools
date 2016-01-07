057cce8a42b5378eefaa10f0cf89ec5e9dcb642a
jebuss <jens.buss@udo.edu>
2016-01-07 18:14:16 +0100

    calculates the division of two arrays.

---
+++ src/main/java/fact/utils/DividePixelArrays.java
@@ -0,0 +1,101 @@
+/**
+ * 
+ */
+package fact.utils;
+
+import fact.Utils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import stream.Data;
+import stream.Processor;
+import stream.annotations.Parameter;
+
+/**
+ * Subtract two Pixel Arrays of the same size from each other
+ * 
+ * @author jbuss
+ *
+ */
+public class DividePixelArrays implements Processor {
+	static Logger log = LoggerFactory.getLogger(RemappingKeys.class);
+	
+    @Parameter(required = true, description = "The key to your data array.")
+    private String numeratorKey;
+    @Parameter(required = true, description = "The key to your subtracted data array.")
+    private String denominatorKey;
+    @Parameter(required = false)
+    private String outputKey;
+    
+	private int npix;
+    
+
+	/* (non-Javadoc)
+	 * @see stream.Processor#process(stream.Data)
+	 */
+	@Override
+	public Data process(Data input) {
+		Utils.isKeyValid(input, numeratorKey, double[].class);
+		Utils.isKeyValid(input, denominatorKey, double[].class);
+		Utils.isKeyValid(input, "NPIX", Integer.class);
+		npix = (Integer) input.get("NPIX");
+		
+		double[] dividedArray =  new double[npix];
+		
+		double[] numerator 	 = (double[]) input.get(numeratorKey);
+		double[] denominator = (double[]) input.get(denominatorKey);
+		
+		for(int pix = 0 ; pix < npix; pix++){
+			if (denominator[pix] != 0){
+				dividedArray[pix] = numerator[pix] / denominator[pix];
+			} else {
+				dividedArray[pix] = 0; //TODO this is a very dirty HACK, for the sake of the viewer we should change it!
+			}
+		}
+		
+		//add times over threshold
+		input.put(outputKey, dividedArray);
+		
+		
+		return input;
+	}
+
+
+
+
+	public String getNumeratorKey() {
+		return numeratorKey;
+	}
+
+
+
+	public void setNumeratorKey(String numeratorKey) {
+		this.numeratorKey = numeratorKey;
+	}
+
+
+
+	public String getDenominatorKey() {
+		return denominatorKey;
+	}
+
+
+
+	public void setDenominatorKey(String denominatorKey) {
+		this.denominatorKey = denominatorKey;
+	}
+
+
+
+	public String getOutputKey() {
+		return outputKey;
+	}
+
+
+
+	public void setOutputKey(String outputKey) {
+		this.outputKey = outputKey;
+	}
+	
+	
+
+}
