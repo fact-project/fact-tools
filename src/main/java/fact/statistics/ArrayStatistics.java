cbda2d70bcd21ce7bbb7c338d44863ff934d1e76
Maximilian Noethe <maximilian.noethe@tu-dortmund.de>
2016-02-09 16:44:20 +0100

    more ArrayStatistics refactoring

--- src/main/java/fact/statistics/ArrayStatistics.java
+++ src/main/java/fact/statistics/ArrayStatistics.java
@@ -11,17 +11,23 @@ import stream.Processor;
 import stream.annotations.Parameter;
 
 /**
- * Writes some information about the given array to the data item. The following information will be written:
+ * Calculates statistical properties about the given array to the data item.
+ * The following properties are calculated:
+ * * median
+ * * mean
+ * * max
+ * * min
+ * * stdDev - standard deviation
+ * * variance
+ * * kurtosis
+ * * skewness
+ * * sum
+ * * pSigmaLow - lower 1 sigma percentile
+ * * pSigmaHigh - upper 1 sigma percentile
+ * * p25 - lower quartile
+ * * p75 - upper quartile
  *
- * outputKey+"_" +"mean"
- * outputKey+"_" +"max"
- * outputKey+"_" +"min"
- * outputKey+"_" +"kurtosis"
- * outputKey+"_" +"variance"
- * outputKey+"_" +"skewness"
- *
- * also a subarray can be defined by a given pixelSet that contains the pixel ids that should be used for the sub array.
- * If no set can be find for the given key NaN are returned for the values.
+ * If <code>pixelSet</code> is given, these values are only calculated for the given pixels.
  *
  * Created by jbuss
  */
@@ -30,9 +36,9 @@ public class ArrayStatistics implements Processor {
     static Logger log = LoggerFactory.getLogger(ArrayStatistics.class);
     @Parameter(required = true, description = "Key to the array you want the information about")
     private String key = null;
-    @Parameter(required = false, description = "The basename for the output, if not given, use inputKey")
+    @Parameter(required = false, description = "The basename for the output, if not given, use 'key:'")
     private String outputKey = null;
-    @Parameter(description = "key of a pixelSet (PixelSetOverlay) containing the IDs of a desired Subset")
+    @Parameter(description = "Key pointing to a pixelSet, if given, only calculate values on these pixels")
     private String pixelSetKey = null;
 
 
@@ -61,10 +67,6 @@ public class ArrayStatistics implements Processor {
             }
         }
         item.put(outputKey + ":" + "median", s.getPercentile(50));
-        item.put(outputKey + ":" + "pSigmaLow", s.getPercentile(15.87));
-        item.put(outputKey + ":" + "pSimgaHigh", s.getPercentile(84.13));
-        item.put(outputKey + ":" + "p25", s.getPercentile(25));
-        item.put(outputKey + ":" + "p75", s.getPercentile(75));
         item.put(outputKey + ":" + "mean", s.getMean());
         item.put(outputKey + ":" + "max", s.getMax());
         item.put(outputKey + ":" + "min", s.getMin());
@@ -73,6 +75,10 @@ public class ArrayStatistics implements Processor {
         item.put(outputKey + ":" + "kurtosis", s.getKurtosis());
         item.put(outputKey + ":" + "skewness", s.getSkewness());
         item.put(outputKey + ":" + "sum", s.getSum());
+        item.put(outputKey + ":" + "pSigmaLow", s.getPercentile(15.87));
+        item.put(outputKey + ":" + "pSimgaHigh", s.getPercentile(84.13));
+        item.put(outputKey + ":" + "p25", s.getPercentile(25));
+        item.put(outputKey + ":" + "p75", s.getPercentile(75));
 
         return item;
 
