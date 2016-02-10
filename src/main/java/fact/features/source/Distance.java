aa7f7a327a035adfba13d9f1bc68715063d1cb86
Fabian Temme <fabian.temme@tu-dortmund.de>
2016-02-10 17:27:33 +0100

    Refactored Distance

--- src/main/java/fact/features/source/Distance.java
+++ src/main/java/fact/features/source/Distance.java
@@ -1,76 +1,46 @@
 package fact.features.source;
 
 import fact.Utils;
-import fact.container.PixelDistribution2D;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import stream.Data;
 import stream.Processor;
 import stream.annotations.Parameter;
 
 /**
- * Quite simply the distance between the CoG of the shower and the calculated source position.
- * @author kaibrugge
+ * Quite simply the distance between the CoG of the shower and the source position.
+ * @author kaibrugge, Fabian Temme
  *
  */
 public class Distance implements Processor {
 	static Logger log = LoggerFactory.getLogger(Distance.class);
-    @Parameter(required = true)
-	private String distribution;
-    @Parameter(required = true)
-	private String sourcePosition;
-    @Parameter(required = true)
-	private String outputKey;
-	/**
-	 * @return input. The original DataItem with a double named {@code outputKey}. Will return null one inputKey was invalid 
-	 */
+   
+	@Parameter(required = false, defaultValue = "shower:ellipse:cog:x")
+	private String cogxKey = "shower:ellipse:cog:x";
+	@Parameter(required = false, defaultValue = "shower:ellipse:cog:y")
+	private String cogyKey = "shower:ellipse:cog:y";
+	@Parameter(required = false, defaultValue="sourcePosition:x")
+	private String sourcePositionXKey = "sourcePosition:x";
+	@Parameter(required = false, defaultValue="sourcePosition:y")
+	private String sourcePositionYKey = "sourcePosition:y";
+	@Parameter(required = false, defaultValue="shower:source:distance")
+	private String outputKey = "shower:source:distance";
+
 	@Override
-	public Data process(Data input) {
-		if(!input.containsKey(distribution)){
-			log.info("No shower in evernt. Not calculating distance");
-			return input;
-		}
-		Utils.mapContainsKeys( input, distribution, sourcePosition);
+	public Data process(Data item) {
+		Utils.mapContainsKeys( item, cogxKey, cogyKey, sourcePositionXKey, sourcePositionYKey);
 
-		PixelDistribution2D dist = (PixelDistribution2D) input.get(distribution);
-		double[] source  = (double[]) input.get(sourcePosition);
+		double sourcex = (Double) item.get(sourcePositionXKey);
+		double sourcey = (Double) item.get(sourcePositionYKey);
 
-		double x = source[0];
-		double y = source[1];
+		double cogx = (Double) item.get(cogxKey);
+		double cogy = (Double) item.get(cogyKey);
+		
+		double distance = Math.sqrt(Math.pow((cogx - sourcex),2) + Math.pow((cogy - sourcey),2));
 
-		input.put(outputKey, 
-				Math.sqrt( (dist.getCenterY() - y) * (dist.getCenterY() - y)
-						+ (dist.getCenterX() - x) * (dist.getCenterX() - x) )
-				);
-		return input;
+		item.put(outputKey, distance);
+		return item;
 	}
-
-
-
-	public String getDistribution() {
-		return distribution;
-	}
-	public void setDistribution(String distribution) {
-		this.distribution = distribution;
-	}
-
-
-
-	public String getSourcePosition() {
-		return sourcePosition;
-	}
-	public void setSourcePosition(String sourcePosition) {
-		this.sourcePosition = sourcePosition;
-	}
-
-
-
-	public String getOutputKey() {
-		return outputKey;
-	}
-	public void setOutputKey(String outputKey) {
-		this.outputKey = outputKey;
-	}
-
-
 }
