33642d0413af12e9c55a7fc5f568373b5a7d7485
jebuss <jens.buss@udo.edu>
2016-02-10 13:11:45 +0100

    deleted call of pos.set*

--- src/test/java/fact/parameter/ParameterTest.java
+++ src/test/java/fact/parameter/ParameterTest.java
@@ -93,9 +93,6 @@ public class ParameterTest {
         dist.process(item);
 
         SourcePosition pos = new SourcePosition();
-        pos.setX(0.0);
-        pos.setY(0.0);
-        pos.setOutputKey(sourcePosition);
         pos.init(null);
         pos.process(item);
     }
