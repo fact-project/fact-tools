14c25561492f3bc03f1bf0c36e4420711f39d7b2
Alexey Egorov <aegorov87@gmail.com>
2016-02-13 23:38:24 +0100

    writing fits better

--- src/main/java/fact/io/FitsWriter.java
+++ src/main/java/fact/io/FitsWriter.java
@@ -45,7 +45,6 @@ public class FitsWriter implements StatefulProcessor {
             } catch (Exception e) {
                 e.printStackTrace();
             }
-//            break;
         }
         try {
             BufferedFile bf = new BufferedFile("test" + counter++ + ".fits", "rw");
@@ -61,26 +60,41 @@ public class FitsWriter implements StatefulProcessor {
         Object[] objects = new Object[2];
         objects[0] = new String[]{key};
         Class<? extends Serializable> type = serialized.getClass();
-        if (ClassUtils.isAssignable(type, String.class)) {
-            String[] strings = {(String) serialized};
-            objects[1] = strings;
-        } else if (ClassUtils.isAssignable(type, Integer.class)) {
-            int[] integers = {(int) serialized};
-            objects[1] = integers;
-        } else if (ClassUtils.isAssignable(type, Double.class)) {
-            double[] doubles = {(double) serialized};
-            objects[1] = doubles;
-        } else if (ClassUtils.isAssignable(type, Float.class)) {
-            float[] floats = {(float) serialized};
-            objects[1] = floats;
-        } else if (ClassUtils.isAssignable(type, Short.class)) {
-            short[] shorts = {(short) serialized};
-            objects[1] = shorts;
-        } else if (ClassUtils.isAssignable(type, Long.class)) {
-            long[] longs = {(long) serialized};
-            objects[1] = longs;
+        if (type.isArray()) {
+            if (serialized instanceof int[]) {
+                int[] arr = (int[]) serialized;
+                fits.addHDU(FitsFactory.hduFactory(arr));
+                return;
+            } else if (serialized instanceof double[]) {
+                double[] arr = (double[]) serialized;
+                fits.addHDU(FitsFactory.hduFactory(arr));
+                return;
+            } else if (serialized instanceof byte[]) {
+                byte[] arr = (byte[]) serialized;
+                objects= new Object[]{key, arr};
+                fits.addHDU(FitsFactory.hduFactory(objects));
+                return;
+            } else {
+                return;
+            }
         } else {
-            return;
+            if (ClassUtils.isAssignable(type, String.class)) {
+                objects[1] = new String[]{(String) serialized};
+            } else if (ClassUtils.isAssignable(type, Integer.class)) {
+                objects[1] = new int[]{(int) serialized};
+            } else if (ClassUtils.isAssignable(type, Double.class)) {
+                objects[1] = new double[]{(double) serialized};
+            } else if (ClassUtils.isAssignable(type, Float.class)) {
+                objects[1] = new float[]{(float) serialized};
+            } else if (ClassUtils.isAssignable(type, Short.class)) {
+                objects[1] = new short[]{(short) serialized};
+            } else if (ClassUtils.isAssignable(type, Long.class)) {
+                objects[1] = new long[]{(long) serialized};
+            } else if (ClassUtils.isAssignable(type, Boolean.class)) {
+                objects[1] = new boolean[]{(boolean) serialized};
+            } else {
+                return;
+            }
         }
         fits.addHDU(FitsFactory.hduFactory(objects));
     }
