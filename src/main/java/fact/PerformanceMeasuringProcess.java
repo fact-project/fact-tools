ac5e5c11186568aecad1029bb3aa66f3d013059c
Maximilian Noethe <maximilian.noethe@tu-dortmund.de>
2016-02-14 19:55:34 +0100

    remove unused import

--- src/main/java/fact/PerformanceMeasuringProcess.java
+++ src/main/java/fact/PerformanceMeasuringProcess.java
@@ -6,7 +6,6 @@ import java.util.HashMap;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
-import org.apache.commons.math3.stat.interval.IntervalUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
