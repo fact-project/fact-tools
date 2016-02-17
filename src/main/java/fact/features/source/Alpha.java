89644aef8632bf756e93fdbf1463e4203998b030
Alexey Egorov <aegorov87@gmail.com>
2016-02-17 12:41:05 +0100

    use sourceKey as single paramter with standard X and Y coordinates for Alpha calculation

--- src/main/java/fact/features/source/Alpha.java
+++ src/main/java/fact/features/source/Alpha.java
@@ -1,59 +1,55 @@
 package fact.features.source;
 
+import fact.Utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import fact.Utils;
 import stream.Data;
 import stream.Processor;
 import stream.annotations.Parameter;
+
 /**
  * This processor calculates the angle between the main axis of the shower and the line cog <-> sourcePosition
- * 
- *@author Kai Bruegge &lt;kai.bruegge@tu-dortmund.de&gt;Fabian Temme
- * 
+ *
+ * @author Kai Bruegge &lt;kai.bruegge@tu-dortmund.de&gt;Fabian Temme
  */
 public class Alpha implements Processor {
-	static Logger log = LoggerFactory.getLogger(Alpha.class);
+    public static final String Y = ":y";
+    public static final String X = ":x";
+    static Logger log = LoggerFactory.getLogger(Alpha.class);
 
-	@Parameter(required = false, defaultValue = "shower:ellipse:cog:x")
-	private String cogxKey = "shower:ellipse:cog:x";
-	@Parameter(required = false, defaultValue = "shower:ellipse:cog:y")
-	private String cogyKey = "shower:ellipse:cog:y";
-	@Parameter(required = false, defaultValue = "shower:ellipse:delta")
-	private String deltaKey = "shower:ellipse:delta";
-	@Parameter(required = false, defaultValue="source:x")
-	private String sourcexKey = "source:x";
-	@Parameter(required = false, defaultValue="source:y")
-	private String sourceyKey = "source:y";
-	@Parameter(required = false, defaultValue="shower:source:alpha")
-	private String outputKey = "shower:source:alpha";
-	
-	@Override
-	public Data process(Data item) {
-		
-		Utils.mapContainsKeys(item, sourcexKey, sourceyKey, cogxKey, cogyKey, deltaKey);
+    @Parameter(required = false, defaultValue = "shower:ellipse:cog")
+    private String cogKey = "shower:ellipse:cog";
+    @Parameter(required = false, defaultValue = "shower:ellipse:delta")
+    private String deltaKey = "shower:ellipse:delta";
+    @Parameter(required = false, defaultValue = "source")
+    private String sourceKey = "source";
+    @Parameter(required = false, defaultValue = "shower:source:alpha")
+    private String outputKey = "shower:source:alpha";
 
-		double sourcex = (Double) item.get(sourcexKey);
-		double sourcey = (Double) item.get(sourceyKey);
+    @Override
+    public Data process (Data item) {
 
-		double cogx = (Double) item.get(cogxKey);
-		double cogy = (Double) item.get(cogyKey);
-		double delta = (Double) item.get(deltaKey);
+        Utils.mapContainsKeys(item,
+                sourceKey + X, sourceKey + Y,
+                cogKey + X, cogKey + Y, deltaKey);
 
-		double alpha = 0.0;
-	    double auxiliary_angle  = Math.atan( (sourcey - cogy )/(sourcex - cogx) );
-	    alpha                  =  (delta - auxiliary_angle);
-	
-	    if (alpha > Math.PI / 2)
-	    {
-	        alpha              = alpha - Math.PI;
-	    }
-	    if (alpha < -Math.PI / 2)
-	    {
-	        alpha              = Math.PI + alpha;
-	    }
-	    item.put(outputKey, alpha);
-		return item;
-	}
+        double sourcex = (Double) item.get(sourceKey + X);
+        double sourcey = (Double) item.get(sourceKey + Y);
+
+        double cogx = (Double) item.get(cogKey + X);
+        double cogy = (Double) item.get(cogKey + Y);
+        double delta = (Double) item.get(deltaKey);
+
+        double auxiliary_angle = Math.atan((sourcey - cogy) / (sourcex - cogx));
+        double alpha = (delta - auxiliary_angle);
+
+        if (alpha > Math.PI / 2) {
+            alpha = alpha - Math.PI;
+        }
+        if (alpha < -Math.PI / 2) {
+            alpha = Math.PI + alpha;
+        }
+        item.put(outputKey, alpha);
+        return item;
+    }
 }
