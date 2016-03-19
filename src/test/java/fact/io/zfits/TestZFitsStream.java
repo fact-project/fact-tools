39621c7ad08a2a8c3b08b399b1938492858d74e3
Kai <kai.bruegge@gmail.com>
2016-03-19 12:18:19 +0100

    fix unused test in zfits io package

--- src/test/java/fact/io/zfits/TestZFitsStream.java
+++ src/test/java/fact/io/zfits/TestZFitsStream.java
@@ -1,5 +1,6 @@
 package fact.io.zfits;
 
+import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import stream.Data;
@@ -10,15 +11,16 @@ import java.net.URL;
 public class TestZFitsStream {
 	static Logger log = LoggerFactory.getLogger(TestZFitsStream.class);
 
-	private final String zfits = "/zfits_test.fz";
-	private final String fits = "/fits_test.gz";
 
-
-	//read all entries
-	public void testReadZFits() throws Exception {
-		URL u =  TestZFitsStream.class.getResource(zfits);
+    /**
+     * Test whether plain fits files can be read correctly
+     * @throws Exception
+     */
+	@Test
+	public void testReadFits() throws Exception {
+		URL u =  TestZFitsStream.class.getResource("/testDataFile.fits.gz");
 		ZFitsStream stream = new ZFitsStream(new SourceURL(u));
-		stream.setTableName("Events");
+		stream.tableName = "Events";
 		stream.init();
 		
 		log.info("Item number {}", 0);
@@ -27,9 +29,11 @@ public class TestZFitsStream {
 				((short[]) item.get("Data")).length 
 				);
 		int i = 1;
-		while (item != null) {
-			log.info("Item number {}", i++);
+        //the test file contains just 15 valid events.
+		while (i < 15) {
 			item = stream.read();
+            log.debug("Item  {} has {} elements",i,  item.size());
+            i++;
 		}
 	}
 }
