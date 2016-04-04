3f5ad81a19450750ec2282a793b4bc51d902a94a
jebuss <jens.buss@udo.edu>
2016-04-04 16:19:03 +0200

    implemented commutative correlation of neighbor pixels

--- src/main/java/fact/extraction/comulativeCovariance.java
+++ src/main/java/fact/extraction/comulativeCovariance.java
@@ -1,6 +1,10 @@
 package fact.extraction;
 
 import fact.Utils;
+import fact.hexmap.CameraPixel;
+import fact.hexmap.FactCameraPixel;
+import fact.hexmap.FactPixelMapping;
+import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
 import stream.Data;
 import stream.ProcessContext;
 import stream.Processor;
@@ -18,7 +22,7 @@ public class comulativeCovariance implements StatefulProcessor {
     @Parameter(required = true)
     private String key = null;
 
-    @Parameter(required = true)
+    @Parameter(required = true, description = "array containing the positions of maximum amplitudes for each pixel")
     private String amplitudePositionsKey = null;
 
     @Parameter(description = "Number of slices to be skipped at the time lines beginning", defaultValue = "50")
@@ -33,6 +37,8 @@ public class comulativeCovariance implements StatefulProcessor {
 
     private int npix = 1440;
 
+    FactPixelMapping pixelMap = FactPixelMapping.getInstance();
+
     @Override
     public Data process(Data input) {
 
@@ -46,15 +52,60 @@ public class comulativeCovariance implements StatefulProcessor {
         int roi = data.length / npix;
         double[] scaledData = data.clone();
 
+        DescriptiveStatistics[] pixelStatistics = new DescriptiveStatistics[npix];
+        double[][] scaledPixelData = new double[npix][];
+
+        double[] comulativeCovariance = new double[npix];
+        double[] comulativeCorelation = new double[npix];
+
         for (int pix = 0; pix < npix; pix++) {
             int maxAmplPos = roi*pix + amplitudePositions[pix];
             double maxAmpl = data[maxAmplPos];
-//            double[] pixData = Arrays.copyOfRange(data,pix*roi, (pix+1)*roi - 1);
 
             for (int slice = 0; slice < roi; slice++) {
                 scaledData[pix*roi+slice] /= maxAmpl;
             }
+
+            scaledPixelData[pix] = Arrays.copyOfRange(scaledData,pix*roi, (pix+1)*roi);
+            pixelStatistics[pix] = new DescriptiveStatistics( scaledPixelData[pix] );
         }
+
+//        Till this point we scaled all timeserise to the max amplitude in each pixel. Now I would like to calculate the covariance of neightborpixel time series
+        for (int pix = 0; pix < npix; pix++) {
+            FactCameraPixel[] neighbours = pixelMap.getNeighboursFromID(pix);
+
+            double pixVariance  = pixelStatistics[pix].getVariance();
+            double pixMean      = pixelStatistics[pix].getMean();
+
+            comulativeCovariance[pix] = 0.;
+            comulativeCorelation[pix] = 0.;
+
+            for (CameraPixel neighbour :
+                    neighbours) {
+
+                double neighbourVariance    = pixelStatistics[neighbour.id].getVariance();
+                double neighbourMean        = pixelStatistics[neighbour.id].getMean();
+
+
+                double covariance = 0.;
+
+                for (int slice = 0; slice < roi; slice++) {
+                    double distancePixel = scaledPixelData[pix][slice] - pixMean;
+                    double distanceNeighbour = scaledPixelData[neighbour.id][slice] - neighbourMean;
+                    covariance += distancePixel * distanceNeighbour;
+                }
+                covariance /= roi;
+
+                comulativeCovariance[pix] += covariance;
+                comulativeCorelation[pix] += covariance / Math.sqrt(pixVariance*pixVariance*neighbourVariance*neighbourVariance );
+            }
+        }
+
+        input.put("comulativeCovariance", comulativeCovariance);
+        input.put("comulativeCorelation", comulativeCorelation);
+
+//            double[] scaledPixelData = Arrays.copyOfRange(scaledData,pix*roi, (pix+1)*roi - 1);
+
         input.put(outputKey, scaledData);
 
         return input;
