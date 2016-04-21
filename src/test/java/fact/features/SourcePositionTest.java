1b928f658bc612816051f6ad674eaf087219bf08
Kai Bruegge <kai.bruegge@udo.edu>
2016-04-21 11:59:05 +0200

    fix test for sourcepostiion by using proper precission deltas

--- src/test/java/fact/features/SourcePositionTest.java
+++ src/test/java/fact/features/SourcePositionTest.java
@@ -3,6 +3,7 @@ package fact.features;
 import fact.Constants;
 import fact.features.source.SourcePosition;
 import fact.hexmap.FactPixelMapping;
+import fact.hexmap.PixelMapping;
 import junit.framework.Assert;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
@@ -21,6 +22,9 @@ import static org.junit.Assert.*;
 
 public class SourcePositionTest {
     static Logger log = LoggerFactory.getLogger(SourcePositionTest.class);
+
+
+    PixelMapping pixelMap = FactPixelMapping.getInstance();
 
     //Adjust the precision for the equatorialToHorizontal (raDec to AzimuthZenith) conversion here.
     double precision = 0.35;
@@ -69,7 +73,7 @@ public class SourcePositionTest {
         double ra = 16.8978379666666;
         double dec = 39.76016913;
         double[] azAlt = sourcePosition.equatorialToHorizontal(ra, dec, dateTime);
-        assertEquals(azAlt[0], -326.45127290084133, precision);
+        assertEquals(azAlt[0], 33.548727099114615, precision);
         assertEquals(azAlt[1], 99.68421165386194, precision);
     }
 
@@ -80,7 +84,7 @@ public class SourcePositionTest {
         double ra = 5.57553886;
         double dec = 22.0145;
         double[] azAlt = sourcePosition.equatorialToHorizontal(ra, dec, dateTime);
-        assertEquals(azAlt[0], -276.12947205268705, precision);
+        assertEquals(azAlt[0], 83.87052808190194, precision);
         assertEquals(azAlt[1], 51.108027267582536, precision);
     }
 
@@ -153,44 +157,41 @@ public class SourcePositionTest {
      *
      * @throws Exception
      */
+//	@Test
+//	public void testCetaTauri() throws Exception
+//	{
+//        SourcePosition sourcePosition = new SourcePosition();
+//        sourcePosition.setOutputKey("test");
+//
+//		double C_T_rightAscension = (5.0 + 37.0/60 + 38.7/3600);
+//		double C_T_declination = 21.0 + 8.0/60 + 33.0/3600;
+//
+//		int[] C_T_chids = {1378,215,212};
+//		double[] C_T_coord = new double[2];
+//
+//		for (int i = 0 ; i < 3 ; i++)
+//		{
+//			C_T_coord[0] += pixelMap.getPixelFromId(C_T_chids[i]).getXPositionInMM();
+//			C_T_coord[1] += pixelMap.getPixelFromId(C_T_chids[i]).getYPositionInMM();
+//		}
+//
+//		C_T_coord[0] /= 3;
+//		C_T_coord[1] /= 3;
+//
+//		double pointingRa = 83.1375/360 * 24;
+//		double pointingDec = 21.628055555555555;
+//		double gmst = 1.1289573103059787;
+//
+//		double[] pointingAzDe = sourcePosition.equatorialToHorizontal(pointingRa, pointingDec, gmst);
+//		double[] sourceAzDe = sourcePosition.equatorialToHorizontal(C_T_rightAscension, C_T_declination, gmst);
+//
+//		double[] sPos =  sourcePosition.getSourcePosition(pointingAzDe[0], pointingAzDe[1], sourceAzDe[0], sourceAzDe[1]);
+//
+//		assertTrue("Calculated coordinates of ceta tauri doesn't fit the coordinates I got from flashy pixels:\n"
+//				+ "Coord_from_pixel: ("+C_T_coord[0]+","+C_T_coord[1]+")\n"
+//				+ "Coord_calculated: ("+sPos[0]+","+sPos[1]+")", (Math.abs(C_T_coord[0]-sPos[0])<Constants.PIXEL_SIZE && Math.abs(C_T_coord[1]-sPos[1])<Constants.PIXEL_SIZE));
+//	}
 
-	/*
-	@Test
-	public void testCetaTauri() throws Exception
-	{
-        SourcePosition sourcePosition = new SourcePosition();
-        sourcePosition.setOutputKey("test");
-
-		double C_T_rightAscension = (5.0 + 37.0/60 + 38.7/3600);
-		double C_T_declination = 21.0 + 8.0/60 + 33.0/3600;
-		
-		int[] C_T_chids = {1378,215,212};
-		double[] C_T_coord = new double[2];
-		
-		for (int i = 0 ; i < 3 ; i++)
-		{
-			C_T_coord[0] += pixelMap.getPixelFromId(C_T_chids[i]).getXPositionInMM();
-			C_T_coord[1] += pixelMap.getPixelFromId(C_T_chids[i]).getYPositionInMM();
-		}
-		
-		C_T_coord[0] /= 3;
-		C_T_coord[1] /= 3;
-		
-		double pointingRa = 83.1375/360 * 24;
-		double pointingDec = 21.628055555555555;
-		double gmst = 1.1289573103059787;
-		
-		double[] pointingAzDe = sourcePosition.equatorialToHorizontal(pointingRa, pointingDec, gmst);
-		double[] sourceAzDe = sourcePosition.equatorialToHorizontal(C_T_rightAscension, C_T_declination, gmst);
-		
-		double[] sPos =  sourcePosition.getSourcePosition(pointingAzDe[0], pointingAzDe[1], sourceAzDe[0], sourceAzDe[1]);
-				
-		assertTrue("Calculated coordinates of ceta tauri doesn't fit the coordinates I got from flashy pixels:\n"
-				+ "Coord_from_pixel: ("+C_T_coord[0]+","+C_T_coord[1]+")\n"
-				+ "Coord_calculated: ("+sPos[0]+","+sPos[1]+")", (Math.abs(C_T_coord[0]-sPos[0])<Constants.PIXEL_SIZE && Math.abs(C_T_coord[1]-sPos[1])<Constants.PIXEL_SIZE));
-	}
-	*/
-	
 	@Test
 	public void testSouthOrientation()
 	{
@@ -203,9 +204,11 @@ public class SourcePositionTest {
 		double azPointing = 0.0;
 		
 		double[] r = sourcePosition.getSourcePosition(azPointing, zdPointing, azSource, zdSource);
-		
-		assertTrue("Calculated position for source in (zd=0.6°,az=0.0) for pointing in(zd=0.0°,az=0.0) is wrong\n"
-				+ "Calculated position: ("+r[0]+","+r[1]+")\n"
-				+ "sought position: (51.20983219603325,-0.0)", (Math.abs(r[0]-51.20983219603325)<1E-14 && Math.abs(r[1]-0.0)<1E-14));		
+		assertEquals("Calculated position for source in (zd=0.6°,az=0.0) for pointing in(zd=0.0°,az=0.0) is wrong " +
+                "Calculated position: (\"+r[0]+\",\"+r[1]+\") \n" +
+                "sought position: (51.20983219603325,-0.0)", 51.20983219603325, r[0], 0.015 );
+        assertEquals("Calculated position for source in (zd=0.6°,az=0.0) for pointing in(zd=0.0°,az=0.0) is wrong " +
+                "Calculated position: (\"+r[0]+\",\"+r[1]+\") \n" +
+                "sought position: (51.20983219603325,-0.0)", 0.0, r[1], 0.0001 );
 	}
 }
