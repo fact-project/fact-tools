d9a3a4a259f9de15268e2d531ade6f746491462d
Fabian Temme <fabian.temme@tu-dortmund.de>
2016-05-03 15:18:38 +0200

    Removed unused imports and removed the commented testCetaTauri. It is not possible to easily adapt this tests to the new calculation methods, and it was also not a perfect test. Their are several new tests, replacing this old one

--- src/test/java/fact/features/SourcePositionTest.java
+++ src/test/java/fact/features/SourcePositionTest.java
@@ -1,30 +1,17 @@
 package fact.features;
 
-import fact.Constants;
 import fact.features.source.SourcePosition;
-import fact.hexmap.FactPixelMapping;
-import fact.hexmap.PixelMapping;
-import junit.framework.Assert;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.TimeZone;
-
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 public class SourcePositionTest {
     static Logger log = LoggerFactory.getLogger(SourcePositionTest.class);
-
-
-//    PixelMapping pixelMap = FactPixelMapping.getInstance();
 
     //Adjust the precision for the equatorialToHorizontal (raDec to AzimuthZenith) conversion here.
     double precision = 0.35;
@@ -153,48 +140,6 @@ public class SourcePositionTest {
         fail("Should have caught an IllegalArgumentException here.");
     }
 
-
-    /**
-     *
-     * For some known az, dec coordinates for Ceta Tauri calculate the corresponding sourceposition
-     * in the camera and compare that to the position of the pixels which are known to show the image of ceta tauri
-     *
-     * @throws Exception
-     */
-//	@Test
-//	public void testCetaTauri() throws Exception
-//	{
-//        SourcePosition sourcePosition = new SourcePosition();
-//        sourcePosition.setOutputKey("test");
-//
-//		double C_T_rightAscension = (5.0 + 37.0/60 + 38.7/3600);
-//		double C_T_declination = 21.0 + 8.0/60 + 33.0/3600;
-//
-//		int[] C_T_chids = {1378,215,212};
-//		double[] C_T_coord = new double[2];
-//
-//		for (int i = 0 ; i < 3 ; i++)
-//		{
-//			C_T_coord[0] += pixelMap.getPixelFromId(C_T_chids[i]).getXPositionInMM();
-//			C_T_coord[1] += pixelMap.getPixelFromId(C_T_chids[i]).getYPositionInMM();
-//		}
-//
-//		C_T_coord[0] /= 3;
-//		C_T_coord[1] /= 3;
-//
-//		double pointingRa = 83.1375/360 * 24;
-//		double pointingDec = 21.628055555555555;
-//		double gmst = 1.1289573103059787;
-//
-//		double[] pointingAzDe = sourcePosition.equatorialToHorizontal(pointingRa, pointingDec, gmst);
-//		double[] sourceAzDe = sourcePosition.equatorialToHorizontal(C_T_rightAscension, C_T_declination, gmst);
-//
-//		double[] sPos =  sourcePosition.getSourcePosition(pointingAzDe[0], pointingAzDe[1], sourceAzDe[0], sourceAzDe[1]);
-//
-//		assertTrue("Calculated coordinates of ceta tauri doesn't fit the coordinates I got from flashy pixels:\n"
-//				+ "Coord_from_pixel: ("+C_T_coord[0]+","+C_T_coord[1]+")\n"
-//				+ "Coord_calculated: ("+sPos[0]+","+sPos[1]+")", (Math.abs(C_T_coord[0]-sPos[0])<Constants.PIXEL_SIZE && Math.abs(C_T_coord[1]-sPos[1])<Constants.PIXEL_SIZE));
-//	}
 
 	@Test
 	public void testSouthOrientation()
