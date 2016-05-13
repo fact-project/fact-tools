519e15e5c624ddd9333a1407a52f894a636c6bdf
Maximilian Noethe <maximilian.noethe@tu-dortmund.de>
2016-05-13 22:15:07 +0200

    new Processor to calculate time resolution with threshold for rings

--- src/main/java/fact/features/muon/RingStandardDeviationWithThreshold.java
+++ src/main/java/fact/features/muon/RingStandardDeviationWithThreshold.java
@@ -1,7 +1,89 @@
 package fact.features.muon;
 
+import com.google.common.collect.Sets;
+import fact.Utils;
+import fact.container.PixelSet;
+import fact.hexmap.CameraPixel;
+import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
+import stream.Data;
+import stream.Processor;
+import stream.annotations.Parameter;
+import streams.tikz.Pixel;
+
 /**
  * Created by max on 13.05.16.
  */
-public class RingStandardDeviationWithThreshold {
+public class RingStandardDeviationWithThreshold implements Processor {
+
+    @Parameter(description = "Key containing the photoncharge array", defaultValue = "photoncharge")
+    private String photonchargeKey = "photoncharge";
+
+    @Parameter(description = "Key containing the arrivalTime array", defaultValue = "arrivalTime")
+    private String arrivalTimeKey = "arrivalTime";
+
+    @Parameter(description = "Key for the ring pixelset", defaultValue = "bestRingPixel")
+    private String ringPixelSetKey = "bestRingPixel";
+
+    @Parameter(description = "Key for the cleaning pixelset", defaultValue = "shower")
+    private String cleaningPixelSetKey = "shower";
+
+    @Parameter(description = "The threshold, only pixels above the threshold are considered", defaultValue = "8")
+    private double threshold = 8.0;
+
+    @Parameter(description = "The outputkey", defaultValue = "StdDevTime{threshold}")
+    private String outputKey = null;
+
+    @Override
+    public Data process(Data item) {
+
+        if (outputKey == null){
+            outputKey = "StdDevTime" + String.valueOf(threshold);
+        }
+
+        Utils.mapContainsKeys(item, photonchargeKey, arrivalTimeKey, ringPixelSetKey, cleaningPixelSetKey);
+
+        PixelSet cleaningPixel = (PixelSet) item.get(cleaningPixelSetKey);
+        PixelSet ringPixel = (PixelSet) item.get(ringPixelSetKey);
+
+        double[] photoncharge = (double[]) item.get(photonchargeKey);
+        double[] arrivalTime = (double[]) item.get(arrivalTimeKey);
+
+
+        Sets.SetView<CameraPixel> intersection = Sets.intersection(cleaningPixel.set, ringPixel.set);
+
+        DescriptiveStatistics stats = new DescriptiveStatistics();
+        for(CameraPixel pixel: intersection){
+            if(photoncharge[pixel.id] >= threshold){
+                stats.addValue(arrivalTime[pixel.id]);
+            }
+        }
+
+        item.put(outputKey, stats.getStandardDeviation());
+        item.put("numPixel" + outputKey, stats.getN());
+        return item;
+    }
+
+    public void setPhotonchargeKey(String photonchargeKey) {
+        this.photonchargeKey = photonchargeKey;
+    }
+
+    public void setArrivalTimeKey(String arrivalTimeKey) {
+        this.arrivalTimeKey = arrivalTimeKey;
+    }
+
+    public void setRingPixelSetKey(String ringPixelSetKey) {
+        this.ringPixelSetKey = ringPixelSetKey;
+    }
+
+    public void setCleaningPixelSetKey(String cleaningPixelSetKey) {
+        this.cleaningPixelSetKey = cleaningPixelSetKey;
+    }
+
+    public void setThreshold(double threshold) {
+        this.threshold = threshold;
+    }
+
+    public void setOutputKey(String outputKey) {
+        this.outputKey = outputKey;
+    }
 }
