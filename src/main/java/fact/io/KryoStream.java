148f9bad80afc299eac25a8370f39aa3e0a68084
Kai Bruegge <kai.bruegge@udo.edu>
2016-06-10 16:01:33 +0200

    kryo ffs

--- src/main/java/fact/io/KryoStream.java
+++ src/main/java/fact/io/KryoStream.java
@@ -1,7 +1,74 @@
 package fact.io;
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.KryoException;
+import com.esotericsoftware.kryo.io.Input;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+
+import stream.Data;
+import stream.data.DataFactory;
+import stream.io.AbstractStream;
+import stream.io.SourceURL;
+import streams.io.LocalDateTimeSerializer;
 
 /**
- * Created by kai on 10.06.16.
+ * KryoStream creates a stream out of an <a href="https://github.com/EsotericSoftware/kryo">Kryo</a>
+ * serialized file.
+ *
+ * @author kai
+ * @see <a href="https://github.com/EsotericSoftware/kryo"/>
  */
-public class KryoStream {
+public class KryoStream extends AbstractStream {
+
+    static Logger log = LoggerFactory.getLogger(KryoStream.class);
+
+    public KryoStream(){}
+
+    public KryoStream(SourceURL url) {
+        super(url);
+    }
+
+    long itemCounter = 0;
+    Input input;
+    Kryo kryo = new Kryo();
+    HashMap<String, Serializable> map = new HashMap<>();
+
+    @Override
+    public void init() throws Exception {
+        super.init();
+
+        input = new Input(url.openStream());
+        kryo.register(LocalDateTime.class, new LocalDateTimeSerializer());
+    }
+
+    @Override
+    public Data readNext() throws Exception {
+        if (itemCounter == limit - 1) {
+            itemCounter = 0;
+            count = 0l;
+            return null;
+        }
+        try {
+            Data item = DataFactory.create(kryo.readObject(input, map.getClass()));
+            itemCounter++;
+            //the kryo files contain keys for datarate which have been produced while writing these files.
+            item.remove("@datarate");
+            return item;
+        } catch (KryoException e) {
+            log.error("Kryo Exception. End of file reached?");
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @Override
+    public void close() throws Exception {
+        super.close();
+        input.close();
+    }
 }
--- src/main/java/fact/io/KryoWriter.java
+++ src/main/java/fact/io/KryoWriter.java
@@ -1,7 +1,56 @@
 package fact.io;
 
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.io.Output;
+
+import java.io.FileOutputStream;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+
+import stream.Data;
+import stream.ProcessContext;
+import stream.StatefulProcessor;
+import stream.annotations.Parameter;
+import stream.io.SourceURL;
+import streams.io.LocalDateTimeSerializer;
+
 /**
- * Created by kai on 10.06.16.
+ * KryoWriter writes DataItem as an Kryo object to a given file.
+ *
+ * @author kai
  */
-public class KryoWriter {
+public class KryoWriter implements StatefulProcessor {
+
+
+    @Parameter(required = true, description = "The url to write to")
+    SourceURL url;
+
+    Kryo kryo;
+    Output output;
+
+    @Override
+    public void init(ProcessContext processContext) throws Exception {
+        System.out.println("Writing to " + url.getPath());
+        output = new Output(new FileOutputStream(url.getFile()));
+        kryo = new Kryo();
+        kryo.register(LocalDateTime.class, new LocalDateTimeSerializer());
+    }
+
+    @Override
+    public void resetState() throws Exception {
+
+    }
+
+    @Override
+    public void finish() throws Exception {
+        output.close();
+    }
+
+    @Override
+    public Data process(Data data) {
+        HashMap<String, Serializable> map = (HashMap<String, Serializable>) data.createCopy();
+        kryo.writeObject(output, map);
+        return data;
+    }
 }
