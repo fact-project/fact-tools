6e807ad7196dcd93d7af672d9a02f3026c65166c
Kai <kai.bruegge@gmail.com>
2016-06-12 16:05:02 +0200

    remove obsolete sqlite service. make auxfielservice capable ofreading aux folder completely

--- src/test/java/fact/rta/WebServiceTest.java
+++ src/test/java/fact/rta/WebServiceTest.java
@@ -3,6 +3,8 @@ package fact.rta;
 import com.google.common.collect.Range;
 import org.joda.time.DateTime;
 import org.junit.Test;
+import org.skife.jdbi.v2.DBI;
+import stream.io.SourceURL;
 
 import java.io.IOException;
 import java.net.URL;
@@ -17,7 +19,11 @@ public class WebServiceTest {
     @Test
     public void startServer() throws SQLException, InterruptedException, IOException {
         URL resource = JDBITest.class.getResource("/data.sqlite");
-        RTAWebService s = new RTAWebService(resource.getPath());
+        RTAWebService s = new RTAWebService();
+        s.sqlitePath = new SourceURL(resource);
+        DBI dbi = new DBI("jdbc:sqlite:" + s.sqlitePath.getPath());
+        RTASignalTable t = dbi.open(RTASignalTable.class);
+        t.createSignalTableIfNotExists();
         for (int i = 0; i < 3600; i++) {
             Thread.sleep(1100);
             Random random = new Random();
@@ -27,11 +33,13 @@ public class WebServiceTest {
             double size = random.nextDouble()*1000;
             double energy = random.nextDouble()*2000;
             DateTime now = DateTime.now();
+            if (i % 30 == 0) {
+                RTAProcessor.SignalContainer c = new RTAProcessor().new SignalContainer(random.nextInt(2), random.nextInt(5), 5, 0.4, 0.2);
+                Range<DateTime> dateTimeRange = Range.closedOpen(now.minusSeconds(15), now.plusSeconds(15));
+                s.updateLightCurve(dateTimeRange, c, "Wow");
+            }
 
-            RTAProcessor.SignalContainer c = new RTAProcessor().new SignalContainer(random.nextInt(2), random.nextInt(5), 5, 0.4, 0.2);
 
-            Range<DateTime> dateTimeRange = Range.closedOpen(now, now.plusSeconds(15));
-            s.updateLightCurve(dateTimeRange, c, "Wow");
 
             s.updateEvent(photons, energy, size, thetaSquare, "Wow", DateTime.now());
             s.updateDataRate(now , new Random().nextDouble()*10);
--- src/test/java/fact/services/AuxServiceTest.java
+++ src/test/java/fact/services/AuxServiceTest.java
@@ -1,14 +1,28 @@
 package fact.services;
 
+import com.google.common.collect.HashBasedTable;
+import fact.auxservice.AuxCache;
+import fact.auxservice.AuxPoint;
 import fact.auxservice.AuxiliaryServiceName;
 import fact.auxservice.AuxFileService;
+import fact.auxservice.strategies.AuxPointStrategy;
+import fact.auxservice.strategies.Closest;
+import org.joda.time.DateTime;
 import org.junit.Test;
 import stream.io.SourceURL;
 
-import java.io.FileNotFoundException;
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.HashMap;
 
+import static com.sun.xml.internal.fastinfoset.alphabet.BuiltInRestrictedAlphabets.table;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNot.not;
+import static org.hamcrest.core.IsNull.nullValue;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -17,32 +31,57 @@ import static org.junit.Assert.assertTrue;
 public class AuxServiceTest {
 
 
-    /**
-     * Tests what happens when the folder to the datestring exists but the files in that folder
-     * don't have the right datestring. We expect an empty map in response.
-     * @throws Exception
-     */
     @Test
-    public void testWrongFilename() throws Exception {
-        URL u = AuxServiceTest.class.getResource("/dummy_files/aux/2015/09/");
-        AuxFileService s = new AuxFileService();
-        //supply a wrong datestring
-//        String dateString = "20150921";
-        HashMap<AuxiliaryServiceName, SourceURL> m =  s.findAuxFileUrls(new SourceURL(u));
-        assertTrue(m.isEmpty());
+    public void testDateTimeToFACTNight(){
+        Integer factNight = AuxCache.dateTimeStampToFACTNight(DateTime.parse("2014-01-02T23:55:02"));
+        assertThat(factNight, is(20140102));
+
+        //now test what happens after 00:00
+        factNight = AuxCache.dateTimeStampToFACTNight(DateTime.parse("2014-01-03T00:55:02"));
+        assertThat(factNight, is(20140102));
+
+        //now test the next day
+        factNight = AuxCache.dateTimeStampToFACTNight(DateTime.parse("2014-01-03T12:00:01"));
+        assertThat(factNight, is(20140103));
+
+        //now test what happens after newyears
+        factNight = AuxCache.dateTimeStampToFACTNight(DateTime.parse("2014-01-01T00:55:02"));
+        assertThat(factNight, is(20131231));
+
+        //now test my birfday!!
+        factNight = AuxCache.dateTimeStampToFACTNight(DateTime.parse("1987-09-20T20:55:02"));
+        assertThat(factNight, is(19870920));
     }
 
 
     @Test
     public void testAuxFileFinder() throws Exception {
-        URL u = AuxServiceTest.class.getResource("/dummy_files/aux/2015/09/21");
+        URL u = AuxServiceTest.class.getResource("/dummy_files/aux/");
+//        SourceURL url = new SourceURL(u);
+        AuxFileService.AuxFileFinder auxFileFinder = new AuxFileService().new AuxFileFinder();
+        Files.walkFileTree(new File(u.getFile()).toPath(), auxFileFinder);
+        HashBasedTable<Integer, AuxiliaryServiceName, Path> table = auxFileFinder.auxFileTable;
+
+        assertThat(table.size(), is(24));
+
+        assertThat(table.contains(20140920, AuxiliaryServiceName.FTM_CONTROL_STATE), is(true));
+
+        assertThat(table.contains(20130102, AuxiliaryServiceName.DRIVE_CONTROL_TRACKING_POSITION), is(true));
+
+        assertThat(table.contains(20140920, AuxiliaryServiceName.DRIVE_CONTROL_POINTING_POSITION), is(false));
+
+        assertThat(table.contains(19870920, AuxiliaryServiceName.FTM_CONTROL_STATE), is(false));
+    }
+
+    @Test
+    public void testAuxFileService() throws Exception {
+        URL u = AuxServiceTest.class.getResource("/dummy_files/aux/");
 //        SourceURL url = new SourceURL(u);
         AuxFileService s = new AuxFileService();
-        //supply datestring. ITS MY BIRFDAY! YAY
-//        String dateString = "20150920";
-        HashMap<AuxiliaryServiceName, SourceURL> m =  s.findAuxFileUrls(new SourceURL(u));
-        assertTrue(m.containsKey(AuxiliaryServiceName.DRIVE_CONTROL_TRACKING_POSITION));
-//        assertTrue(m.containsKey("DRIVE_CONTROL_POINTING_POSITION"));
+        s.auxFolder = new SourceURL(u);
+        AuxPoint auxiliaryData = s.getAuxiliaryData(AuxiliaryServiceName.DRIVE_CONTROL_TRACKING_POSITION, DateTime.parse("2013-01-02T23:30:21"), new Closest());
+        assertThat(auxiliaryData, is(not(nullValue())));
+        assertThat(auxiliaryData.getDouble("Ra"), is(not(nullValue())));
     }
 
 }
