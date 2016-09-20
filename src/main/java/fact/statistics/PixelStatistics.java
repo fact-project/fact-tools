879dcb273e5fad2a09928d4d3dbfdc7b92d6c70a
jbuss <jens.buss@udo.edu>
2016-09-20 14:16:34 +0200

    implemented based on processor time row features. Deleted anything not neccessary

--- src/main/java/fact/statistics/PixelStatistics.java
+++ src/main/java/fact/statistics/PixelStatistics.java
@@ -1,6 +1,7 @@
 package fact.statistics;
 
 import fact.Utils;
+import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.math3.stat.StatUtils;
 import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
 import org.apache.commons.math3.stat.descriptive.rank.Percentile;
@@ -8,38 +9,23 @@ import stream.Data;
 import stream.Processor;
 import stream.annotations.Parameter;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 
 /**
- * This processor calculates several features of the timerow in a given timewindow:
- * - mean, std, skewness, kurtosis
- * - median, mode, min, max, quantil 25%, quantil 75%
- * - histogram (over all events) from the difference between the data voltages and the voltages after a moving average filter
+ * Calculate the Statistics of Pixel Arrays
  * 
- * @author F. Temme
+ * @author Jens Buss
  *
  */
 public class PixelStatistics implements Processor {
 	
 	@Parameter(required=true,description="key to the data array")
 	private String dataKey = null;
-	@Parameter(required=true,description="key to a data array, which was previously filtered using a moving average filter")
-	private String movingAverageKey = null;
-	@Parameter(required=false,description="left side of the search window for which the features are calculated. 0 < searchWindowLeft < roi", defaultValue="10")
-	private int searchWindowLeft = 10;
-	@Parameter(required=false,description="right side of the search window for which the features are calculated. 0 < searchWindowLeft < searchWindowRight < roi", defaultValue="250")
-	private int searchWindowRight = 250;
 	@Parameter(required=true,description="name of the key of the calculated features")
 	private String outputKey = null;
-	
-	
-	private int numberOfBins = 200;
-	private double histogramMinBin = -10.0;
-	private double histogramMaxBin = 10.0;
-	private double binWidth = (histogramMaxBin - histogramMinBin)/numberOfBins;
-	
 
-	private int[] histogram = new int[numberOfBins+2];
-	
 	private int npix;
 	
 	@Override
@@ -47,15 +33,9 @@ public class PixelStatistics implements Processor {
 		Utils.isKeyValid(input, "NPIX", Integer.class);
 		npix = (Integer) input.get("NPIX");
 		
-		int roi = (Integer) input.get("NROI");
-		
-		Utils.checkWindow(searchWindowLeft, searchWindowRight-searchWindowLeft, 0, roi);
-		
-		Utils.mapContainsKeys(input, dataKey, movingAverageKey);
-		
-		double[] data = (double[]) input.get(dataKey);
-		double[] movingAverage = (double[]) input.get(movingAverageKey);
-		
+		Utils.mapContainsKeys(input, dataKey);
+
+		ArrayList<ArrayList<Integer>> data = (ArrayList<ArrayList<Integer>>) input.get(dataKey);
 		
 		double[] mean = new double[npix];
 		double[] median = new double[npix];
@@ -68,16 +48,15 @@ public class PixelStatistics implements Processor {
 		double[] quantil25 = new double[npix];
 		double[] quantil75 = new double[npix];
 		
-		for (int pix = 0 ; pix < npix ; pix++)
-		{
-			double[] values = new double[searchWindowRight - searchWindowLeft]; 
-			for (int sl = searchWindowLeft ; sl < searchWindowRight ; sl++)
-			{
-				int slice = pix*roi + sl;
-				values[sl-searchWindowLeft] = data[slice];
-				int binNumber = findBinNumber((data[slice]-movingAverage[slice]));
-				histogram[binNumber] += 1;
+		for (int pix = 0 ; pix < npix ; pix++){
+			ArrayList<Integer> list = data.get(pix);
+			int[] intArray = ArrayUtils.toPrimitive(list.toArray(new Integer[list.size()]));
+			double[] values = Arrays.stream(intArray).asDoubleStream().toArray();
+
+			if (values.length == 0){
+				continue;
 			}
+
 			DescriptiveStatistics stats = new DescriptiveStatistics(values);
 			mean[pix] = stats.getMean();
 			min[pix] = stats.getMin();
@@ -85,11 +64,9 @@ public class PixelStatistics implements Processor {
 			std[pix] = stats.getStandardDeviation();
 			skewness[pix] = stats.getSkewness();
 			kurtosis[pix] = stats.getKurtosis();
-			
-			Percentile percentile = new Percentile();
-			median[pix] = percentile.evaluate(values,0.5);
-			quantil25[pix] = percentile.evaluate(values, 0.25);
-			quantil75[pix] = percentile.evaluate(values, 0.75);
+			quantil25[pix] = stats.getPercentile(0.25);
+			quantil75[pix] = stats.getPercentile(0.75);
+			median[pix] = stats.getPercentile(0.5);
 			
 			double[] modeArray = StatUtils.mode(values);
 			mode[pix] = modeArray[0];
@@ -105,24 +82,11 @@ public class PixelStatistics implements Processor {
 		input.put(outputKey+ "_max", max);
 		input.put(outputKey+ "_quantil25", quantil25);
 		input.put(outputKey+ "_quantil75", quantil75);
-		input.put(outputKey+ "_histogram",histogram);
 		
 		
 		return input;
 	}
-	
-	private int findBinNumber(double value){
-		if (value < histogramMinBin)
-		{
-			return 0;
-		}
-		if (value >= histogramMaxBin)
-		{
-			return numberOfBins+1;
-		}
-		int binNumber = (int) ( (value - histogramMinBin) / binWidth);
-		return binNumber;
-	}
+
 
 	public String getDataKey() {
 		return dataKey;
@@ -130,30 +94,6 @@ public class PixelStatistics implements Processor {
 
 	public void setDataKey(String dataKey) {
 		this.dataKey = dataKey;
-	}
-
-	public String getMovingAverageKey() {
-		return movingAverageKey;
-	}
-
-	public void setMovingAverageKey(String movingAverageKey) {
-		this.movingAverageKey = movingAverageKey;
-	}
-
-	public int getSearchWindowLeft() {
-		return searchWindowLeft;
-	}
-
-	public void setSearchWindowLeft(int searchWindowLeft) {
-		this.searchWindowLeft = searchWindowLeft;
-	}
-
-	public int getSearchWindowRight() {
-		return searchWindowRight;
-	}
-
-	public void setSearchWindowRight(int searchWindowRight) {
-		this.searchWindowRight = searchWindowRight;
 	}
 
 	public String getOutputKey() {
@@ -164,28 +104,5 @@ public class PixelStatistics implements Processor {
 		this.outputKey = outputKey;
 	}
 
-	public int getNumberOfBins() {
-		return numberOfBins;
-	}
-
-	public void setNumberOfBins(int numberOfBins) {
-		this.numberOfBins = numberOfBins;
-	}
-
-	public double getHistogramMinBin() {
-		return histogramMinBin;
-	}
-
-	public void setHistogramMinBin(double histogramMinBin) {
-		this.histogramMinBin = histogramMinBin;
-	}
-
-	public double getHistogramMaxBin() {
-		return histogramMaxBin;
-	}
-
-	public void setHistogramMaxBin(double histogramMaxBin) {
-		this.histogramMaxBin = histogramMaxBin;
-	}
 
 }
