d5f643b90cf8912cb5929a6c74ee1ed7c11b8c38
Kai <kai.bruegge@gmail.com>
2016-11-04 18:46:44 +0100

    add fits class for abstracting a file that works on gzipped streams

--- src/main/java/fact/io/hdureader/Fits.java
+++ src/main/java/fact/io/hdureader/Fits.java
@@ -1,8 +1,6 @@
 package fact.io.hdureader;
 
-import com.google.common.collect.ArrayListMultimap;
-import com.google.common.collect.HashBasedTable;
-import com.google.common.collect.Table;
+import com.google.common.collect.ImmutableList;
 import com.google.common.io.ByteStreams;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,42 +8,40 @@ import org.slf4j.LoggerFactory;
 import java.io.*;
 import java.net.URL;
 import java.util.*;
+import java.util.zip.GZIPInputStream;
 
 /**
  * Created by mackaiver on 03/11/16.
  */
 public class Fits {
-    static Logger log = LoggerFactory.getLogger(Fits.class);
+    private static Logger log = LoggerFactory.getLogger(Fits.class);
 
     private final int HDULIMIT = 1000;
 
-    final URL url;
+    private final URL url;
     private final List<HDU>  hdus = new ArrayList<>();
 
     //this is a mapping from extname
-    Map<String, HDU> t = new HashMap<>();
-    Map<HDU, Long> m = new HashMap<>();
+    private Map<String, HDU> hduNames = new HashMap<>();
 
     public HDU primaryHDU;
 
+
     public Fits(URL  url) throws IOException {
         this.url = url;
-        DataInputStream stream = new DataInputStream(new BufferedInputStream(url.openStream()));
+        DataInputStream stream = getDecompressedDataStream(url.openStream());
 
-        Long positionInStream = 0L;
         try {
             while (hdus.size() < HDULIMIT) {
                 HDU h = new HDU(stream);
                 hdus.add(h);
 
-                Long positionOfDataArea = positionInStream + h.size;
+
                 h.get("EXTNAME").ifPresent(name -> {
-                    t.put(name, h);
-                    m.put(h, positionOfDataArea);
+                    hduNames.put(name, h);
                 });
 
                 ByteStreams.skipFully(stream, h.offsetToNextHDU());
-                positionInStream += h.offsetToNextHDU();
 
             }
         } catch (EOFException e){
@@ -55,6 +51,38 @@ public class Fits {
         primaryHDU = hdus.get(0);
     }
 
+    private DataInputStream getDecompressedDataStream(InputStream stream) throws IOException {
+
+        byte[] header = new byte[2];
+        stream.mark(2);
+
+        int read = stream.read(header);
+        if(read != 2){
+            throw new IOException("Could not read the first 2 bytes from stream.");
+        }
+
+        //reset stream to start
+        stream.reset();
+
+        //check if matches standard gzip magic number
+        if(isGzippedCompressed(header)) {
+            log.info("Getting gzipped stream");
+            return new DataInputStream(new BufferedInputStream(new GZIPInputStream(stream)));
+        }else {
+            return new DataInputStream(new BufferedInputStream(stream));
+        }
+    }
+
+    /**
+     * Given the first 2 bytes of an input stream, this returns whether the stream is
+     * gzipped or not
+     * @param header byte[] containing the first two bytes of any file/stream
+     * @return true iff stream is gzipped.
+     */
+    public static boolean isGzippedCompressed(byte[] header){
+        return header[0] == (byte) GZIPInputStream.GZIP_MAGIC  && header[1] == (byte) (GZIPInputStream.GZIP_MAGIC >> 8);
+    }
+
     /**
      * Each extension has a keyword called EXTNAME. This method returns the extension with the given name if it exists.
      *
@@ -62,22 +90,42 @@ public class Fits {
      * @return the HDU with the passed EXTNAME value
      */
     public Optional<HDU> getHDU(String extname){
-        return Optional.ofNullable(t.get(extname));
+        return Optional.ofNullable(hduNames.get(extname));
     }
 
     public InputStream getInputStreamForHDUData(String hduExtName) throws IOException {
-        if(!t.containsKey(hduExtName)){
+        if(!hduNames.containsKey(hduExtName)){
             throw new IOException("Extension with name " + hduExtName + " not found in file" );
         }
-        return getInputStreamForHDUData(t.get(hduExtName));
+        return getInputStreamForHDUData(hduNames.get(hduExtName));
     }
 
-    public InputStream getInputStreamForHDUData(HDU h) throws IOException {
-        InputStream stream = url.openStream();
-        long skipped = stream.skip(m.get(h));
-        if(m.get(h) != skipped){
-            log.warn("Could not skip to data area.");
+    /**
+     *
+     * @param hdu
+     * @return
+     * @throws IOException
+     */
+    public DataInputStream getInputStreamForHDUData(HDU hdu) throws IOException {
+        DataInputStream stream = getDecompressedDataStream(url.openStream());
+
+
+        long bytesToSkip = 0;
+        for(HDU h : hdus){
+            if(h.equals(hdu)){
+                bytesToSkip += h.headerSizeInBytes;
+                break;
+            }
+            bytesToSkip += h.headerSizeInBytes + h.sizeOfDataArea();
         }
+
+
+        long skipped = stream.skip(bytesToSkip);
+        if(bytesToSkip!= skipped ){
+            log.error("Could not skip to data area. Skipped only {} instead of requested {} bytes.", skipped, bytesToSkip);
+            throw new IOException("Could not skip to data area.");
+        }
+
         return stream;
     }
 }
