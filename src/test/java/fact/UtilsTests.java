b8ef8820cae5777c9b82c385f5c2ea84a4cd0ebb
jens buß <jens.buss@tu-dortmund.de>
2016-11-14 13:29:43 +0100

    add unit test for empty array

--- src/test/java/fact/UtilsTests.java
+++ src/test/java/fact/UtilsTests.java
@@ -1,7 +1,17 @@
 package fact;
+import junit.framework.Assert;
+import org.junit.Test;
 
 /**
  * Created by jebuss on 14.11.16.
  */
 public class UtilsTests {
+    @Test
+    public void flattenEmpty2dArray(){
+
+        double[][] empty = new double[0][0];
+        double[] result = Utils.flatten2dArray(empty);
+        Assert.assertEquals(result.length, 0);
+    }
+
 }
