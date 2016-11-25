5f33ea6106956f225e738b24350f89b078fb561c
jens buß <jens.buss@tu-dortmund.de>
2016-11-25 13:26:10 +0100

    delete unsed import

--- src/main/java/fact/Utils.java
+++ src/main/java/fact/Utils.java
@@ -17,7 +17,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
-import java.util.stream.DoubleStream;
 
 import org.apache.commons.lang3.ArrayUtils;
 /**
