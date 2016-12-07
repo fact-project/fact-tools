edc853caf3e237c7f9959399bf480bbf2cdc7d32
Kai B <kai.bruegge@tu-dortmund.de>
2016-12-07 20:18:15 +0100

    have the auxservice only check for specific file locations

--- src/main/java/fact/auxservice/AuxFileService.java
+++ src/main/java/fact/auxservice/AuxFileService.java
@@ -26,13 +26,15 @@ import static java.nio.file.FileVisitResult.CONTINUE;
 import static java.nio.file.FileVisitResult.SKIP_SUBTREE;
 
 /**
- * This implements an AuxiliaryService {@link fact.auxservice.AuxiliaryService}  providing data from the auxiliary
- * files written by the telescopes data acquisition system.
+ * This implements an AuxiliaryService {@link fact.auxservice.AuxiliaryService}  providing data from
+ * the auxiliary files written by the telescopes data acquisition system.
  *
  * Given the path to the aux folder, that is the folder containing all the auxiliary files for FACT.
  * This service will read the requested data and store them in a map of {@link fact.auxservice.AuxPoint}.
  *
- * So far only aux files newer than 2012 are supported!
+ * Only aux files newer than 2012 are supported!
+ *
+ * Simply provide the 'auxFolder' url to the basepath of the aux files e.g. '/fact/aux/'
  *
  * Created by kaibrugge on 07.10.14.
  */
@@ -40,10 +42,8 @@ public class AuxFileService implements AuxiliaryService {
 
     private Logger log = LoggerFactory.getLogger(AuxFileService.class);
 
-    private DateTime lastRefresh = DateTime.now();
-
-
-    @Parameter(required = true, description = "The path to the folder containing the auxilary data as .fits files")
+    @Parameter(required = true, description = "The url pointing to the path containing a the auxilary " +
+            "data in FACTS canonical folder structure." )
     public SourceURL auxFolder;
     public void setAuxFolder(SourceURL auxFolder) {
         this.auxFolder = auxFolder;
@@ -57,24 +57,10 @@ public class AuxFileService implements AuxiliaryService {
             .build(new CacheLoader<AuxCache.CacheKey, TreeSet<AuxPoint>>() {
                 @Override
                 public TreeSet<AuxPoint> load(AuxCache.CacheKey key) throws Exception {
-                    if(lastRefresh.plusHours(12).isAfter(DateTime.now())){
-                        init();
-                    }
                     return readDataFromFile(key);
                 }
             });
 
-
-    private boolean isInit = false;
-    private HashBasedTable<Integer, AuxiliaryServiceName, Path> auxFileUrls;
-
-    private void init() throws IOException {
-        AuxFileFinder auxFileFinder = new AuxFileFinder();
-        Path p = new File(auxFolder.getPath()).toPath();
-        Files.walkFileTree(p, auxFileFinder);
-        auxFileUrls = auxFileFinder.auxFileTable;
-        isInit = true;
-    }
 
     /**
      * This method returns an AuxPoint according to the strategy and the time stamp passed to this method.
@@ -91,14 +77,12 @@ public class AuxFileService implements AuxiliaryService {
      */
     @Override
     public synchronized AuxPoint getAuxiliaryData(AuxiliaryServiceName serviceName, DateTime eventTimeStamp, AuxPointStrategy strategy) throws IOException {
-        if(!isInit) {
-            init();
-        }
+
         if(eventTimeStamp.isAfterNow()){
             log.warn("The requested timestamp seems to be in the future.");
         }
         try {
-            AuxCache.CacheKey key = new AuxCache().new CacheKey(serviceName, AuxCache.dateTimeStampToFACTNight(eventTimeStamp));
+            AuxCache.CacheKey key = new AuxCache().new CacheKey(serviceName, eventTimeStamp);
 
             TreeSet<AuxPoint> auxPoints = cache.get(key);
             AuxPoint pointFromTreeSet = strategy.getPointFromTreeSet(auxPoints, eventTimeStamp);
@@ -114,38 +98,27 @@ public class AuxFileService implements AuxiliaryService {
 
 
     /**
-     * This method returns an AuxPoint according to the strategy and the time stamp passed to this method.
-     * This is useful for getting the source position from the drive files for example. It can work like this:
+     * This method returns an AuxPoints for a whole night according to the strategy and the time stamp passed to this method.
      *
-     *      AuxPoint trackingPoint = auxService.getAuxiliaryData(AuxiliaryServiceName.DRIVE_CONTROL_TRACKING_POSITION, timeStamp, closest);
-     *      double ra = trackingPoint.getDouble("Ra");
-     *
-     * @throws IOException when no auxpoint can be found for given timestamp
+     * @throws IOException when no auxpoint can be found for given night
      */
     public synchronized SortedSet<AuxPoint> getAuxiliaryDataForWholeNight(AuxiliaryServiceName serviceName, DateTime night) throws IOException {
-        if(!isInit) {
-            init();
-        }
-        Integer factNight = AuxCache.dateTimeStampToFACTNight(night);
         try {
-            AuxCache.CacheKey key = new AuxCache().new CacheKey(serviceName, factNight);
+            AuxCache.CacheKey key = new AuxCache().new CacheKey(serviceName, night);
 
             TreeSet<AuxPoint> auxPoints = cache.get(key);
             if (auxPoints.isEmpty()){
-                throw new IOException("No auxpoints found for the given night" + factNight);
+                throw new IOException("No auxpoints found for the given night " + night);
             }
             return auxPoints;
 
         } catch (ExecutionException e) {
-            throw new IOException("No auxpoints found for the given night" + factNight);
-        } catch (IllegalArgumentException e){
-            throw new IOException("From DateTime needs to be earlier than To DateTime");
+            throw new IOException("No auxpoints found for the given night" + night);
         }
-
     }
 
     private TreeSet<AuxPoint>  readDataFromFile(AuxCache.CacheKey key) throws Exception {
-        Path pathToFile = auxFileUrls.get(key.factNight, key.service);
+        Path pathToFile = Paths.get(auxFolder.getPath(), key.path.toString());
         if(pathToFile == null){
             log.error("Could not load auxfile {} for night {}", key.service, key.factNight);
             throw new IOException("Could not load auxfile for key " +  key);
@@ -174,80 +147,6 @@ public class AuxFileService implements AuxiliaryService {
 
     @Override
     public void reset() throws Exception {
-    }
-
-
-
-    /**
-     * A {@code FileVisitor} that finds
-     * all files that match the specified pattern for aux files
-     * Auxfilenames are seperated by dots as in: date.servicename.fits
-     */
-    public class AuxFileFinder extends SimpleFileVisitor<Path> {
-
-        public HashBasedTable<Integer, AuxiliaryServiceName, Path> auxFileTable = HashBasedTable.create();
-
-        // Check file name for proper aux file naming scheme.
-        void matchAuxFile(Path file) {
-            try {
-                Path name = file.getFileName();
-                if (name != null) {
-                    String[] split = name.toString().split("\\.");
-                    //an aux file contains two dots.
-                    if (split.length != 3) {
-                        return;
-                    }
-                    //aux files have a fits extension
-                    if(!split[2].equals("fits")){
-                        return;
-                    }
-                    //get date string (night)
-                    int night = Integer.decode(split[0]);
-                    AuxiliaryServiceName serviceName = AuxiliaryServiceName.valueOf(split[1]);
-                    auxFileTable.put(night, serviceName, file);
-                }
-            } catch(NumberFormatException e){
-                log.warn("Cannot decode night from file with name: " + file.toString());
-            } catch(IllegalArgumentException e){
-                log.info("The file " + file + " is not a recognized auxservice");
-            }
-        }
-
-
-        // Invoke the pattern matching
-        // method on each file.
-        @Override
-        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
-            matchAuxFile(file);
-            return CONTINUE;
-        }
-
-        //check whether the directory name only contains numbers
-        @Override
-        public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) {
-            if (dir.getFileName().toString().equals("aux")){
-                return CONTINUE;
-            }
-            try {
-                short number = Short.parseShort(dir.getFileName().toString());
-                if (number == 2011 || number == 2012){
-                    log.info("Aux files from 2011 and 2012 are not supported");
-                    throw new NumberFormatException();
-                }
-            } catch (NumberFormatException e) {
-                log.info("Directory is not a valid aux directory with name: " + dir.toString() + ".  Skipping subtree.");
-                return SKIP_SUBTREE;
-            }
-            return CONTINUE;
-        }
-
-
-        @Override
-        public FileVisitResult visitFileFailed(Path file, IOException exc) {
-            log.error("An error occured while trying to read directories " + exc.getLocalizedMessage() +
-                    "\n Continuing with next file.");
-            return CONTINUE;
-        }
     }
 
 }
