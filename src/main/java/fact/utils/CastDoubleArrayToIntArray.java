9b4eaf25f606ec416f1142929aee996ea381b58d
Dominik Neise <neised@phys.ethz.ch>
2017-01-16 18:40:55 +0100

    add: "ElementwiseMultiplyDoubleArray" and "CastDoubleArrayToIntArray"

---
+++ src/main/java/fact/utils/CastDoubleArrayToIntArray.java
@@ -0,0 +1,40 @@
+package fact.utils;
+
+import stream.Data;
+import stream.Processor;
+import stream.annotations.Parameter;
+
+public class CastDoubleArrayToIntArray implements Processor {
+
+    @Parameter(
+        required = true,
+        description = "Key to your double array."
+    )
+    private String inputKey;
+
+    @Parameter(
+        required = true,
+        description = "Key to the output integer array."
+    )
+    protected String outputKey;
+
+    @Override
+    public Data process(Data input) {
+        double[] doubleArray = (double[]) input.get(inputKey);
+        int[] intArray = new int[doubleArray.length];
+
+        for (int i=0; i<intArray.length; ++i)
+            intArray[i] = (int) Math.round(doubleArray[i]);
+
+        input.put(outputKey, intArray);
+        return input;
+    }
+
+    public void setInputKey(String inputKey) {
+        this.inputKey = inputKey;
+    }
+    public void setOutputKey(String outputKey) {
+        this.outputKey = outputKey;
+    }
+
+}
