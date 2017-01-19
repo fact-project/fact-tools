c0f1d9ee192678b23621e24c540cfc58b70abce7
sebastian <sebmuell@phys.ethz.ch>
2017-01-19 21:11:23 +0100

    try to solve issue #150, #190

---
+++ src/main/java/fact/extraction/AboveThreshold.java
@@ -0,0 +1,72 @@
+package fact.extraction;
+
+import fact.Utils;
+import stream.Data;
+import stream.Processor;
+import stream.annotations.Parameter;
+import java.util.Arrays;
+import java.util.ArrayList;
+
+/**
+ * Finds pixels above a certain threshold limit. If only a single slice of the 
+ * pixel is above the threshold, this pixel is marked.
+ * Also the ratio of pixels above the threshold is calculated.
+ */
+public class AboveThreshold implements Processor {
+    @Parameter(
+        required = true, 
+        description = "A double array with length NROI times NPIX"
+    )
+    private String dataKey = null;
+
+    @Parameter(
+        required = true,
+        description = "A list of the pixels above the threshold. "+
+                      "Also the fraction of pixels above the threshold" 
+    )
+    private String outputKey = null;
+
+    @Parameter(
+        required = true,
+        description = "Threshold value to be exceeded"
+    )
+    protected short threshold = 1800;
+
+    @Override
+    public Data process(Data input) {
+
+        final int npix = (Integer) input.get("NPIX");
+        final int roi = (Integer) input.get("NROI");
+        final short[] timeSerieses = (short[]) input.get(dataKey);
+
+        ArrayList<Integer> pixelsAboveThresholdList = new ArrayList<>();
+        for (int pix=0; pix<npix; pix++) {
+            for(int slice=0; slice<roi; slice++) {
+                final int pos = pix*roi + slice;
+                if(timeSerieses[pos] > threshold) {
+                    pixelsAboveThresholdList.add(pix);
+                    break;
+                }
+            }
+        }
+
+        int[] pixelsAboveThreshold = Utils.arrayListToInt(pixelsAboveThresholdList);
+        final double ratioOfPixels = (double)pixelsAboveThreshold.length/(double)npix;
+
+        input.put(outputKey, pixelsAboveThreshold);
+        input.put(outputKey+"Ratio", ratioOfPixels);
+        return input;
+    }
+
+    public void setDataKey(String dataKey) {
+        this.dataKey = dataKey;
+    }
+
+    public void setOutputKey(String outputKey) {
+        this.outputKey = outputKey;
+    }
+
+    public void setThreshold(short threshold) {
+        this.threshold = threshold;
+    }
+}
---
+++ src/test/java/fact/features/AboveThresholdTest.java
@@ -0,0 +1,134 @@
+package fact.features;
+
+import fact.extraction.AboveThreshold;
+import fact.Utils;
+import stream.data.DataFactory;
+import stream.Data;
+import junit.framework.Assert;
+import org.junit.Test;
+
+
+public class AboveThresholdTest {
+
+    @Test
+    public void testAboveThresholdAllZero() {
+
+        Data data = DataFactory.create();
+        short[] raw_fact_data = new short[1440*300];
+
+        data.put("data", raw_fact_data);
+        data.put("NROI", 300);
+        data.put("NPIX", 1440);
+
+        AboveThreshold above = new AboveThreshold();
+        above.setDataKey("data");
+        above.setOutputKey("output");
+        above.setThreshold((short)1);
+        above.process(data);
+        int[] pixelsAboveThreshold = (int[]) data.get("output");
+        double ratio = (double) data.get("outputRatio");
+        Assert.assertEquals(0.0, ratio);
+        Assert.assertEquals(0, pixelsAboveThreshold.length);
+    }
+
+    @Test
+    public void testAboveThresholdCertainPixelsAbove() {
+
+        Data data = DataFactory.create();
+        short[] raw_fact_data = new short[1440*300];
+
+        for(int pix=0; pix<1440; pix++) {
+            for(int slice=0; slice<300; slice++) {
+                final int pos = pix*300 + slice;
+                if(pix==42 || pix==314 || pix==1337) {
+                    raw_fact_data[pos] = 2000;
+                }else{
+                    raw_fact_data[pos] = 2;
+                }
+            }
+        }
+
+        data.put("data", raw_fact_data);
+        data.put("NROI", 300);
+        data.put("NPIX", 1440);
+
+        AboveThreshold above = new AboveThreshold();
+        above.setDataKey("data");
+        above.setOutputKey("output");
+        above.setThreshold((short)5);
+        above.process(data);
+
+        int[] pixelsAboveThreshold = (int[]) data.get("output");
+        double ratio = (double) data.get("outputRatio");
+        Assert.assertEquals(3.0/1440.0, ratio);
+        Assert.assertEquals(3, pixelsAboveThreshold.length);
+        Assert.assertEquals(42, pixelsAboveThreshold[0]);
+        Assert.assertEquals(314, pixelsAboveThreshold[1]);
+        Assert.assertEquals(1337, pixelsAboveThreshold[2]);
+    }
+
+    @Test
+    public void testAboveThresholdAllPixelsAbove() {
+
+        Data data = DataFactory.create();
+        short[] raw_fact_data = new short[1440*300];
+
+        for(int i=0; i<1440*300; i++) {
+            raw_fact_data[i] = 2000;
+        }
+
+        data.put("data", raw_fact_data);
+        data.put("NROI", 300);
+        data.put("NPIX", 1440);
+
+        AboveThreshold above = new AboveThreshold();
+        above.setDataKey("data");
+        above.setOutputKey("output");
+        above.setThreshold((short)5);
+        above.process(data);
+
+        int[] pixelsAboveThreshold = (int[]) data.get("output");
+        double ratio = (double) data.get("outputRatio");
+        Assert.assertEquals(1440.0/1440.0, ratio);
+        Assert.assertEquals(1440, pixelsAboveThreshold.length);
+
+        for(int i=0; i<1440; i++) {
+            Assert.assertEquals(i, pixelsAboveThreshold[i]);
+        }
+    }
+
+    @Test
+    public void testAboveThresholdJustBelow() {
+
+        Data data = DataFactory.create();
+        short[] raw_fact_data = new short[1];
+
+        raw_fact_data[0] = 10;
+
+        data.put("data", raw_fact_data);
+        data.put("NROI", 1);
+        data.put("NPIX", 1);
+
+        AboveThreshold at = new AboveThreshold();
+        at.setDataKey("data");
+        at.setOutputKey("output");
+        at.setThreshold((short)10);
+        at.process(data);
+
+        int[] pixelsAboveThreshold = (int[]) data.get("output");
+        double ratio = (double) data.get("outputRatio");
+        Assert.assertEquals(0.0, ratio);
+        Assert.assertEquals(0, pixelsAboveThreshold.length);
+
+        AboveThreshold at2 = new AboveThreshold();
+        at2.setDataKey("data");
+        at2.setOutputKey("output");
+        at2.setThreshold((short)9);
+        at2.process(data);
+
+        pixelsAboveThreshold = (int[]) data.get("output");
+        ratio = (double) data.get("outputRatio");
+        Assert.assertEquals(1.0, ratio);
+        Assert.assertEquals(1, pixelsAboveThreshold.length);
+    }
+}
