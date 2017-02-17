36ecd21006b18a345fd2c56f8e96e50745ce88e7
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2017-02-17 14:11:46 +0100

    Check ZNAXIS2 for number of events, fix #219

--- src/main/java/fact/io/hdureader/BinTable.java
+++ src/main/java/fact/io/hdureader/BinTable.java
@@ -157,7 +157,7 @@ public class BinTable {
         });
 
 
-        numberOfRowsInTable = header.getInt("NAXIS2").orElse(0);
+        numberOfRowsInTable = header.getInt("ZNAXIS2").orElse(getInt("NAXIS2").orElse(0));
         numberOfColumnsInTable = columns.size();
 
 
