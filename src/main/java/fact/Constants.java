e433edbf01be35e151c4e6a4eb4368e75127d421
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2017-05-23 18:12:24 +0200

    Add focalLength and FACTLocation to Constants

--- src/main/java/fact/Constants.java
+++ src/main/java/fact/Constants.java
@@ -1,11 +1,14 @@
 package fact;
 
+import fact.coordinates.EarthLocation;
+
 public class Constants {
 
 
 	//number of pixels
 	public static final int NUMBEROFPIXEL= 1440;
-	
+	public static final double focalLength = 4889.0;
+	public static final EarthLocation FACTLocation = EarthLocation.fromDegrees(28.761795, -17.890701389, 2200);
 
 
 	public static final double PIXEL_SIZE = 9.5; //9.5mm
