67802c3a2ed20afa933aff2f411ecc02a962ab9f
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2017-05-24 10:19:41 +0200

    Add euclideanDistance to CameraCoordinate

---
+++ src/test/java/fact/coordinates/CameraCoordinateTests.java
@@ -0,0 +1,40 @@
+package fact.coordinates;
+
+import fact.Constants;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by maxnoe on 23.05.17.
+ */
+public class CameraCoordinateTests {
+
+    @Test
+    public void euclideanDistanceTest(){
+        CameraCoordinate c1 = new CameraCoordinate(-1, -1);
+        CameraCoordinate c2 = new CameraCoordinate(1, 1);
+        assertEquals(Math.sqrt(8), c1.euclideanDistance(c2), 1e-12);
+    }
+
+    @Test
+    public void testToHorizontal(){
+        CameraCoordinate c;
+        HorizontalCoordinate h;
+        HorizontalCoordinate pointing = HorizontalCoordinate.fromDegrees(90, 0);
+
+        c = new CameraCoordinate(50.0, 0.0);
+
+        h = c.toHorizontal(pointing, Constants.focalLength);
+
+        assertTrue(h.getZenithDeg() > 90);
+        assertEquals(0.0, h.getAzimuthDeg(), 1e-12);
+
+        c = new CameraCoordinate(0, 50.0);
+        h = c.toHorizontal(pointing, Constants.focalLength);
+
+        assertEquals(90.0, h.getZenithDeg(), 1e-12);
+        assertTrue(h.getAzimuthDeg() > 0);
+    }
+}
