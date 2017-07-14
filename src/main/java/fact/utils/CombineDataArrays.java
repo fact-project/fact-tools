b89f24e9e1a9c7817e89771c69a8385a71852a4a
Michael Bulinski <michael.bulinski@tu-dortmund.de>
2017-07-14 06:22:34 +0200

    added prozessor to combine to double arrays

---
+++ src/main/java/fact/utils/CombineDataArrays.java
@@ -0,0 +1,101 @@
+/**
+ * 
+ */
+package fact.utils;
+
+import fact.Utils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import stream.Data;
+import stream.Processor;
+import stream.annotations.Parameter;
+
+/**
+ * Subtract two Pixel Arrays of the same size from each other
+ * 
+ * @author  Michael Bulinski &lt;michael.bulinski@udo.edu&gt;
+ *
+ */
+public class CombineDataArrays implements Processor {
+	static Logger log = LoggerFactory.getLogger(RemappingKeys.class);
+	
+    @Parameter(required = true, description = "The key to your first array.")
+    private String firstArrayKey;
+    @Parameter(required = true, description = "The key to your second array.")
+    private String secondArrayKey;
+    @Parameter(required = false)
+    private String outputKey;
+    @Parameter(required = true, description = "The operation to perform, (add, sub, mul, div)")
+	private String op;
+    
+	private int npix;
+    
+
+	/* (non-Javadoc)
+	 * @see stream.Processor#process(stream.Data)
+	 */
+	@Override
+	public Data process(Data input) {
+		Utils.isKeyValid(input, firstArrayKey, double[].class);
+		Utils.isKeyValid(input, secondArrayKey, double[].class);
+		
+		double[] resultArray =  new double[npix];
+		
+		double[] array1 	 = (double[]) input.get(firstArrayKey);
+		double[] array2 	 = (double[]) input.get(secondArrayKey);
+
+		if (op=="add") {
+			for (int pix = 0; pix < npix; pix++) {
+				resultArray[pix] = array1[pix] + array2[pix];
+			}
+		} else if (op=="sub") {
+			for (int pix = 0; pix < npix; pix++) {
+				resultArray[pix] = array1[pix] - array2[pix];
+			}
+		} else if (op=="mul") {
+			for (int pix = 0; pix < npix; pix++) {
+				resultArray[pix] = array1[pix] * array2[pix];
+			}
+		} else if (op=="div") {
+			for (int pix = 0; pix < npix; pix++) {
+				resultArray[pix] = array1[pix] / array2[pix];
+			}
+		} else {
+			throw new RuntimeException("The given operation op: "+op+" is not supported");
+		}
+
+		input.put(outputKey, resultArray);
+		
+		return input;
+	}
+
+
+
+	public static Logger getLog() {
+		return log;
+	}
+
+
+
+	public static void setLog(Logger log) {
+		CombineDataArrays.log = log;
+	}
+
+
+	public void setFirstArrayKey(String key) {
+		this.firstArrayKey = key;
+	}
+
+	public void setSecondArrayKey(String key) {
+		this.secondArrayKey = key;
+	}
+
+	public void setOutputKey(String outputKey) {
+		this.outputKey = outputKey;
+	}
+
+	public void setOp(String op) { this.op = op;}
+	
+	
+
+}
