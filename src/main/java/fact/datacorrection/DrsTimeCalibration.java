58a5d9f69c481417bab45962e6f2a3fca3ec183b
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2017-11-15 19:51:13 +0100

    Merge DrsTimeCalibration and ArrayTimeCorrection, fixes #254, #253

--- src/main/java/fact/datacorrection/DrsTimeCalibration.java
+++ src/main/java/fact/datacorrection/DrsTimeCalibration.java
@@ -1,11 +1,12 @@
-package fact.filter;
+package fact.datacorrection;
 
 import fact.Utils;
-import fact.datacorrection.DrsCalibration;
 import fact.io.hdureader.BinTable;
 import fact.io.hdureader.BinTableReader;
 import fact.io.hdureader.FITS;
 import fact.io.hdureader.OptionalTypesMap;
+import fact.utils.LinearTimeCorrectionKernel;
+import fact.utils.TimeCorrectionKernel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import stream.Data;
@@ -21,22 +22,25 @@ import java.net.URL;
 public class DrsTimeCalibration implements StatefulProcessor{
 	static Logger log = LoggerFactory.getLogger(DrsCalibration.class);
 
-	@Parameter(required=false,description="Key of the StartCellData in the data fits file",defaultValue="StartCellData")
+	@Parameter(required=false, description="Key of the StartCellData in the data fits file",defaultValue="StartCellData")
 	private String startCellKey = "StartCellData";
-	@Parameter(required=true,description="Key of the time calibration constants (relative to the start cell of each pixel)")
-	private String outputKey = null;
+
 	@Parameter(required=false, description="name of column in FITS file to find DRS4 time calibration constants.")
 	private String drsTimeKey = "CellOffset";
+
 	@Parameter(required = false, description = "file with the drs time calib constants", defaultValue="classpath:/long_term_constants_median.time.drs.fits")
 	private URL url = DrsTimeCalibration.class.getResource("/long_term_constants_median.time.drs.fits");
 
+	@Parameter(required = true, description = "key to the drs amplitude calibrated voltage curves")
+	private String dataKey = null;
+
+	@Parameter(required = true, description = "OutputKey for the calibrated voltage curves")
+	private String outputKey = null;
+
 	private int numberOfSlices = 1024;
-	private int numberOfTimemarker = 160;
+	private int numberOfTimeMarker = 160;
 
-	private int npix;
-
-
-	private double[] absoluteTimeOffsets = new double[numberOfSlices*numberOfTimemarker];
+	private double[] absoluteTimeOffsets = new double[numberOfSlices * numberOfTimeMarker];
 
 
 	@Override
@@ -52,25 +56,51 @@ public class DrsTimeCalibration implements StatefulProcessor{
 	@Override
 	public Data process(Data input) {
 		Utils.isKeyValid(input, "NPIX", Integer.class);
-		Utils.mapContainsKeys(input, startCellKey, "NROI");
 
-		npix = (Integer) input.get("NPIX");
+		int npix = (Integer) input.get("NPIX");
 		int roi = (Integer) input.get("NROI");
 		short[] startCell = (short[]) input.get(startCellKey);
-		if (startCell==null) {
+
+		if (startCell == null) {
 			throw new RuntimeException("Couldn't find StartCellData");
 		}
-		double[] relativeTimeOffsets = new double[roi*npix];
-		for (int px = 0 ; px < npix ; px++){
+
+		double[] relativeTimeOffsets = new double[roi* npix];
+		for (int px = 0; px < npix; px++){
 			int patch = px / 9;
 			double offsetAtStartCell = absoluteTimeOffsets[patch*numberOfSlices + startCell[px]];
 			for (int slice = 0 ; slice < roi ; slice++){
 				int cell = patch*numberOfSlices + (slice + startCell[px])%numberOfSlices;
-				relativeTimeOffsets[px*roi+slice] = absoluteTimeOffsets[cell]-offsetAtStartCell;
+				relativeTimeOffsets[px * roi + slice] = absoluteTimeOffsets[cell] - offsetAtStartCell;
 			}
 		}
 
-		input.put(outputKey, relativeTimeOffsets);
+		npix = (Integer) input.get("NPIX");
+		double[] data = (double[]) input.get(dataKey);
+		roi = data.length / npix;
+		TimeCorrectionKernel tcKernel = new LinearTimeCorrectionKernel();
+
+		double [] calibratedValues = new double[roi * npix];
+		for(int chid = 0; chid < npix; chid++)
+		{
+			double [] realtimes = new double[roi];
+			double [] values = new double[roi];
+
+			for(int slice = 0; slice < roi; slice++)
+			{
+				realtimes[slice] = slice - relativeTimeOffsets[chid * roi + slice];
+				values[slice] = data[chid * roi + slice];
+			}
+			tcKernel.fit(realtimes, values);
+
+			for(int slice = 0; slice < roi; slice++)
+			{
+				calibratedValues[chid * roi + slice] = tcKernel.interpolate((double) slice);
+			}
+
+		}
+
+		input.put(outputKey, calibratedValues);
 		return input;
 	}
 
