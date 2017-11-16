e3ec6638eea42f219e92099ea1360fc1f6ff1dbc
Jens Buss <jens.buss@online.de>
2017-11-16 13:46:01 +0100

    split up process function into several functions. Use full timeseries, do not ignore margins

--- src/main/java/fact/TriggerEmulation/SumUpPatches.java
+++ src/main/java/fact/TriggerEmulation/SumUpPatches.java
@@ -41,41 +41,63 @@ public class SumUpPatches implements Processor {
         int roi = (int) item.get("NROI");
 
         int n_patches = Constants.NUMBEROFPIXEL/9;
-        int skipFirst = 20;
-        int skipLast = 40;
 
-        double[][] pixel_data = Utils.snipPixelData(data, skipFirst, skipLast, Constants.NUMBEROFPIXEL, roi);
+
+        double[][] pixel_data = Utils.snipPixelData(data, 0, 0, Constants.NUMBEROFPIXEL, roi);
         double[][] patch_sums = new double[n_patches][];
+
         for (int patch = 0; patch < n_patches; patch++) {
-            double[] patch_sum = new double[roi];
-
-            for (int pix = 0; pix < 9; pix++) {
-                int current_pix = patch * 9 + pix;
-                double[] current_patch_sum = patch_sum;
-
-                IntStream.range(skipFirst, current_patch_sum.length-skipLast-skipFirst)
-                        .forEach(
-                                i -> current_patch_sum[i] = pixel_data[current_pix][i] + current_patch_sum[i]
-                        );
-
-                patch_sum = current_patch_sum;
-            }
-            patch_sums[patch] = patch_sum;
+            patch_sums[patch] = sumPixelsOfPatch(pixel_data, patch);
         }
         item.put(outKey, patch_sums);
 
-        if (visualize == true){
-            double[] new_data = new double[0];
-            for (int patch = 0; patch < n_patches; patch++) {
-                for (int pix = 0; pix < 9; pix++) {
-
-                    new_data = ArrayUtils.addAll(new_data, Arrays.copyOf(patch_sums[patch],roi));
-                }
-            }
-            item.put(outKey+"_vis", new_data);
+        if (visualize){
+            item.put(outKey+"_vis", toDataArray(patch_sums));
         }
         return item;
 
+    }
+
+    /**
+     * Convert to a full ROI double array with length npixels*ROI
+     * @param patch_sums
+     * @return
+     */
+    public double[] toDataArray(double[][] patch_sums) {
+        double[] new_data = new double[0];
+
+
+        for (int patch = 0; patch < patch_sums.length; patch++) {
+            for (int pix = 0; pix < 9; pix++) {
+                new_data = ArrayUtils.addAll(new_data, patch_sums[patch]);
+            }
+        }
+        return new_data;
+    }
+
+    /**
+     * sum up all pixels of a given patch
+     * @param pixel_data
+     * @param patch
+     * @return sum of timeserieses of pixels of given patch
+     */
+    public double[] sumPixelsOfPatch(double[][] pixel_data, int patch) {
+        int nPixPerPatch = 9;
+        double[] patch_sum = new double[pixel_data[nPixPerPatch*patch].length];
+        Arrays.fill(patch_sum, 0.);
+        for (int pix = 0; pix < 9; pix++) {
+            int current_pix = patch * 9 + pix;
+
+            assert (patch_sum.length == pixel_data[current_pix].length);
+
+
+            for (int i = 0; i < patch_sum.length; i++) {
+                assert (i < patch_sum.length);
+                assert (i < pixel_data[current_pix].length);
+                patch_sum[i] += pixel_data[current_pix][i];
+            }
+        }
+        return patch_sum;
     }
 
     public void setKey(String key) {
