87707dacab0723e753b8f3ee05104889105d6d00
Michael Bulinski <michael.bulinski@tu-dortmund.de>
2017-11-24 12:54:30 +0100

    create a shellscript to execute the tester with a random amount of files

---
+++ testSubsetZFits.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# executes the ZFitsTester with a random set of files from the rawfolder
+
+if [ "$#" -ne 2 ]; then
+    echo "Illegal number of parameters, expects: rawfolder (eg: /fact/raw) and amount (eg: 100)"
+    exit
+fi
+
+rawFolder=$1
+
+if [ -d "$rawFolder" ]; then
+  echo $rawFolder is not a folder
+  echo Stopping
+  exit
+fi
+
+amount=$2
+
+echo Getting Zfits Files out of: $rawFolder
+echo Getting $amount random files
+
+# get the required amount of random files
+FILES=$(ls $rawFolder/*/*/*/*.fits.?z | sort -R | head -n $amount)
+
+# execute the tester
+java -cp target/fact-tools-0.18.1-SNAPSHOT.jar fact.ZFitsTester $FILES
+
