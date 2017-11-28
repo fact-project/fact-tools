82ce896d16b4cefe338be03e558d6c7acf34127a
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2017-11-28 15:30:31 +0100

    Implemnt writing of keys to the FITSWriter header

--- src/test/java/fact/io/FITSWriterTest.java
+++ src/test/java/fact/io/FITSWriterTest.java
@@ -3,10 +3,7 @@
  */
 package fact.io;
 
-import fact.io.hdureader.BinTable;
-import fact.io.hdureader.BinTableReader;
-import fact.io.hdureader.FITS;
-import fact.io.hdureader.OptionalTypesMap;
+import fact.io.hdureader.*;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,7 +15,6 @@ import java.io.File;
 import java.net.URL;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
 
 import static org.junit.Assert.assertEquals;
 
@@ -38,7 +34,7 @@ public class FITSWriterTest {
         log.info(f.getAbsolutePath());
 
         URL url = new URL("file:" + f.getAbsolutePath());
-        fitsWriter.setUrl(url);
+        fitsWriter.url = url;
         fitsWriter.init(null);
         fitsWriter.finish();
     }
@@ -50,8 +46,8 @@ public class FITSWriterTest {
         log.info(f.getAbsolutePath());
 
         URL url = new URL("file:" + f.getAbsolutePath());
-        fitsWriter.setUrl(url);
-        fitsWriter.setKeys(new Keys("*"));
+        fitsWriter.url = url;
+        fitsWriter.keys = new Keys("*");
 
         fitsWriter.init(null);
 
@@ -69,7 +65,7 @@ public class FITSWriterTest {
         item.put("timestamp", ZonedDateTime.of(2017, 1, 1, 12, 0, 0, 991300000, ZoneOffset.UTC));
 
         fitsWriter.process(item);
-        assertEquals(fitsWriter.getNumEventsWritten(), 1);
+        assertEquals(fitsWriter.numEventsWritten, 1);
         fitsWriter.finish();
 
         FITS fits = FITS.fromFile(f);
@@ -87,5 +83,50 @@ public class FITSWriterTest {
             assertEquals(array[i], arrayRead[i], 1e-12);
         }
         assertEquals(row.get("timestamp"), "2017-01-01T12:00:00.991300Z");
+    }
+
+    @Test
+    public void testHeaderKeys() throws Exception {
+        FITSWriter fitsWriter = new FITSWriter();
+        File f = File.createTempFile("test_fits", ".fits");
+        log.info(f.getAbsolutePath());
+
+        URL url = new URL("file:" + f.getAbsolutePath());
+        fitsWriter.url = url;
+        fitsWriter.keys = new Keys("EventNum,TriggerType");
+        fitsWriter.headerKeys = new Keys("NROI,NPIX,UTCTIME,muchtoolong");
+        fitsWriter.init(null);
+
+        Data item = DataFactory.create();
+        item.put("muchtoolong", true);
+        item.put("EventNum", 1);
+        item.put("TriggerType", 4);
+        item.put("NROI", 300);
+        item.put("NPIX", 1440);
+        item.put("UTCTIME", ZonedDateTime.of(2017, 1, 1, 12, 0, 0, 991300000, ZoneOffset.UTC));
+
+
+        fitsWriter.process(item);
+        assertEquals(fitsWriter.numEventsWritten, 1);
+        fitsWriter.finish();
+
+        FITS fits = FITS.fromFile(f);
+
+        HDU hdu = fits.getHDU("Events");
+
+        Header header = hdu.header;
+
+        // test truncation of too long key
+        assertEquals(true, header.getBoolean("muchtool").get());
+
+        int nroi = header.getInt("NROI").get();
+        assertEquals(300, nroi);
+
+        assertEquals("2017-01-01T12:00:00.991300Z", header.get("UTCTIME").get());
+
+        int npix = header.getInt("NPIX").get();
+        assertEquals(1440, npix);
+
+        assertEquals(false, header.get("EventNum").isPresent());
     }
 }
