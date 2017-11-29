242b099063b04fa0f7cccd39516f3805ded81c1d
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2017-11-29 12:40:18 +0100

    Remove getters and setters, move comment into javadoc

--- src/main/java/fact/features/source/ReconstructSourcePositionDisp.java
+++ src/main/java/fact/features/source/ReconstructSourcePositionDisp.java
@@ -43,11 +43,22 @@ public class ReconstructSourcePositionDisp implements Processor {
         return input;
     }
 
+    /**
+     * The orientation of the reconstructed source position depends on the third moment
+     * (relativ to the suspected source position, m3l*sign(cosDeltaAlpha)) of the shower:
+     * If it is larger than a constant (default -200) the reconstructed source position is
+     * orientated towards the suspected source position
+     *
+     * @param cogx
+     * @param cogy
+     * @param disp
+     * @param delta
+     * @param cosDeltaAlpha
+     * @param m3l
+     * @return CameraCoordinate with the reconstructed source position
+     */
     private CameraCoordinate calculateRecPosition(double cogx, double cogy, double disp, double delta, double cosDeltaAlpha, double m3l) {
-        // The orientation of the reconstructed source position depends on the third moment
-        // (relativ to the suspected source position, m3l*sign(cosDeltaAlpha)) of the shower:
-        // If it is larger than a constant (default -200) the reconstructed source position is
-        // orientated towards the suspected source position
+
         double sign = - Math.signum(cosDeltaAlpha) * Math.signum(m3l * Math.signum(cosDeltaAlpha) - signM3lConstant);
 
         double x = cogx + disp * Math.cos(delta) * sign;
@@ -55,37 +66,4 @@ public class ReconstructSourcePositionDisp implements Processor {
 
         return new CameraCoordinate(x, y);
     }
-
-    public void setDispKey(String dispKey) {
-        this.dispKey = dispKey;
-    }
-
-    public void setCogxKey(String cogxKey) {
-        this.cogxKey = cogxKey;
-    }
-
-    public void setCogyKey(String cogyKey) {
-        this.cogyKey = cogyKey;
-    }
-
-    public void setDeltaKey(String deltaKey) {
-        this.deltaKey = deltaKey;
-    }
-
-    public void setM3lKey(String m3lKey) {
-        this.m3lKey = m3lKey;
-    }
-
-    public void setOutputKey(String outputKey) {
-        this.outputKey = outputKey;
-    }
-
-    public void setCosDeltaAlphaKey(String cosDeltaAlphaKey) {
-        this.cosDeltaAlphaKey = cosDeltaAlphaKey;
-    }
-
-    public void setSignM3lConstant(double signM3lConstant) {
-        this.signM3lConstant = signM3lConstant;
-    }
-
 }
