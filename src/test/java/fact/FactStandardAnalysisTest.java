557b8bff5df8270fd74177aee1b1aef32f24cc82
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2017-12-01 15:51:54 +0100

    Fix indents

--- src/test/java/fact/FactStandardAnalysisTest.java
+++ src/test/java/fact/FactStandardAnalysisTest.java
@@ -1,15 +1,10 @@
 package fact;
 
-import fact.io.FITSStreamTest;
 import fact.io.hdureader.BinTable;
-import fact.io.hdureader.BinTableReader;
 import fact.io.hdureader.FITS;
-import fact.io.hdureader.OptionalTypesMap;
 import org.junit.Test;
 
 import java.io.File;
-import java.net.URI;
-import java.net.URL;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
@@ -18,16 +13,16 @@ import static org.junit.Assert.fail;
 public class FactStandardAnalysisTest {
 
 
-	@Test
-	public void dataTest() {
+    @Test
+    public void dataTest() {
 
-		try {
-			File xml = new File("examples/stdAnalysis/data/analysis.xml");
-			File outFile = File.createTempFile("facttools_test", ".fits");
+        try {
+            File xml = new File("examples/stdAnalysis/data/analysis.xml");
+            File outFile = File.createTempFile("facttools_test", ".fits");
 
             String[] args = {xml.toURI().toString(), "-Doutfile=" + outFile.toURI().toString()};
 
-			stream.run.main(args);
+            stream.run.main(args);
 
             FITS fits = FITS.fromFile(outFile);
             BinTable table = fits.getBinTableByName("Events").get();
@@ -35,11 +30,11 @@ public class FactStandardAnalysisTest {
             assertEquals(3, (long) table.numberOfRowsInTable);
 
 
-		} catch (Exception e) {
-			e.printStackTrace();
-			fail("Could not run the ./fact_example.xml");
-		}
-	}
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail("Could not run the ./fact_example.xml");
+        }
+    }
 
 
     @Test
