86ea5aac4d80a6c55782e22288cb146932cff9d8
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2017-12-05 16:07:24 +0100

    Use own runner for fact-tools, fixes #280

---
+++ src/main/java/fact/run.java
@@ -0,0 +1,31 @@
+package fact;
+
+public class run {
+    public static void main(String[] args) throws Exception {
+        handleArguments(args);
+        stream.run run = new stream.run();
+        run.main(args);
+
+    }
+
+    public static void handleArguments(String[] args) {
+        if (args.length == 0) {
+            System.out.println("fact-tools, version " + VersionInformation.getInstance().version);
+            System.out.println();
+            System.out.println("No container file specified.");
+            System.out.println();
+            System.out.println("Usage: ");
+            System.out.println("\tjava -jar <fact-tools-jar> /path/container-file.xml");
+            System.out.println();
+            System.exit(0);
+        }
+
+        for (String arg : args) {
+            if (arg.equals("-v") || "--version".equals(args)) {
+                System.out.println("version: " + VersionInformation.getInstance().version);
+                System.out.println("git commit: " + VersionInformation.getInstance().commit);
+                System.exit(0);
+            }
+        }
+    }
+}
