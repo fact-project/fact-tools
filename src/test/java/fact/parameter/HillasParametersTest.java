7ae42421354f98c7bebf9dc7032f2e3dbde0f723
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2018-01-16 16:38:13 +0100

    Adapt test to new feature names

--- src/test/java/fact/parameter/HillasParametersTest.java
+++ src/test/java/fact/parameter/HillasParametersTest.java
@@ -14,26 +14,21 @@ public class HillasParametersTest extends ParameterTest {
 
     @Rule
     public ExpectedException thrown = ExpectedException.none();
-
     final String shower = "shower";
-
-    final String outputKey = "distribution";
 
     @Test
     public void testValidParameter() throws Exception {
-//		//start processor with the correct parameter
-        assertTrue("Expecteds output already in data item", !item.containsKey(outputKey));
+        // Start processor with the correct parameter
 
         HillasParameters poser = new HillasParameters();
         poser.pixelSetKey = shower;
         poser.weightsKey = photonCharge;
         poser.process(item);
 
-        String[] keys = {"size", "width", "length", "cog", "cogX", "cogY", "delta", "m3Long", "m3Trans", "m4Long", "m4Trans", "skewnessLong", "skewnessTrans"};
+        String[] keys = {"size", "width", "length", "cog", "cog_x", "cog_y", "delta", "m3_long", "m3_trans", "m4_long", "m4_trans", "skewness_long", "skewness_trans"};
         for (String key: keys){
             assertTrue("Expected output not in data item but it should be there", item.containsKey(key));
         }
 
-//		item.remove(outputKey);
     }
 }
