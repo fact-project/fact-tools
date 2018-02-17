f8fcddae3dfc457b1e3d4d680c8da75a1d1dbb6f
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2018-02-17 12:39:55 +0100

    Throw exception if column type cannot be parsed, add test

--- src/test/java/fact/io/hdureader/BinTableTests.java
+++ src/test/java/fact/io/hdureader/BinTableTests.java
@@ -11,6 +11,7 @@ import java.time.ZonedDateTime;
 import java.util.Map;
 
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
@@ -19,6 +20,17 @@ import static org.junit.Assert.assertTrue;
  * Created by mackaiver on 14/12/16.
  */
 public class BinTableTests {
+
+    @Test
+    public void testEmptyRepeatCount() throws Exception {
+        URL u = BinTableTests.class.getResource("/testRepeatCount.fits.gz");
+        FITS f = new FITS(u);
+        BinTable table = f.getBinTableByName("Events").orElseThrow(IOException::new);
+
+        for (BinTable.TableColumn column: table.columns) {
+            assertEquals(1, column.repeatCount);
+        }
+    }
 
     @Test
     public void testBinTableIterator() throws Exception {
