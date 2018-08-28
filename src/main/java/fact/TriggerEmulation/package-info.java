ef7b9ab01785e03803a977302ea6de227b2da63f
Jens Buss <jens.buss@online.de>
2018-08-28 13:38:34 +0200

    add package info

---
+++ src/main/java/fact/TriggerEmulation/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @author Jens Buß, jens.buss@tu-dortmund.de
+ * This package collects processors to emulate the hardware trigger and trigger logic of FACT.
+ */
+package fact.TriggerEmulation;
