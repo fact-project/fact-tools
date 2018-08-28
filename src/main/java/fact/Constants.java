f4853cff5298508f28fdd399fc6a73e835927bef
Jens Buss <jens.buss@online.de>
2018-08-28 14:03:16 +0200

    whitespace

--- src/main/java/fact/Constants.java
+++ src/main/java/fact/Constants.java
@@ -1,7 +1,7 @@
 package fact;
 
 public class Constants {
-	  public static final double MILLIVOLT_PER_DAC = 610e-3;
+    public static final double MILLIVOLT_PER_DAC = 610e-3;
     public static final int N_PIXELS = 1440;
     public static final double FOCAL_LENGTH_MM = 4889.0;
     public static final double PIXEL_SIZE_MM = 9.5;
