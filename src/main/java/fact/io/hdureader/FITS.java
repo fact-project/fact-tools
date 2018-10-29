998ce73e252b7e697169ba2b2892938de1ead66a
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2018-10-29 15:26:43 +0100

    Log reading file only to debug output

--- src/main/java/fact/io/hdureader/FITS.java
+++ src/main/java/fact/io/hdureader/FITS.java
@@ -83,7 +83,7 @@ public class FITS {
     public FITS(URL url) throws IOException {
         this.url = url;
         DataInputStream stream = getUnGzippedDataStream(url);
-        log.info("Reading file: "+url.toString());
+        log.debug("Reading file: {}", url);
         long absoluteHduOffsetInFile = 0;
 
         try {
