672d9a8d34b1e20df506c499bd450078c23dd608
Jens Buss <jens.buss@tu-dortmund.de>
2018-10-31 14:25:52 +0100

    Expanded description.
    Co-Authored-By: tarrox <tarrox@hotmail.de>

--- src/main/java/fact/utils/SamplePedestalEvent.java
+++ src/main/java/fact/utils/SamplePedestalEvent.java
@@ -56,7 +56,7 @@ public class SamplePedestalEvent implements StatefulProcessor {
                                             "If multivalue: Each value is the length of a single step starting with zenith=0.")
     private String[] binning;
 
-    @Parameter(required = true, description = "The binning key of the double value from the noise database.")
+    @Parameter(required = true, description = "The binning key of the double value from the noise database. e.g. key of the Zd value in the provided noise DB. A bin will be chosen according to the itemBinningKey")
     private String dbBinningKey;
 
     @Parameter(required = true, description = "The binning key of the double value from the item.")
